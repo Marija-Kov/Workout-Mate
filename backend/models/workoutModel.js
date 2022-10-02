d599c8198102415ecfa7199758b3b57ad608dd0a
Marija K <kccmrj@outlook.com>
2022-10-02 21:40:35 +0200

    Create workoutModel.js

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema; 
+// Schema is like a mongoose object property.
+
+const workoutSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required:true
+    },
+    load: {
+        type: Number,
+        required: true
+    }
+
+}, {timestamps: true})
+
+
+module.exports = mongoose.model('Workout', workoutSchema);
