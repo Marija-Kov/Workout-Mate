e077eb4d317708615c2c9b3a46c9832ccc386921
Marija K <kccmrj@outlook.com>
2022-10-02 21:42:07 +0200

    Update server.js
    Connecting to database using URI.

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 require('dotenv').config();
 
-
+const mongoose = require('mongoose');
 
 const routes = require('./routes/workouts');
 
 const app = express();
+
+
+
+app.use(express.json());
 
 app.use((req, res, next)=>{
     console.log(req.path, req.method);
     next();
 })
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`listening on port ${process.env.PORT}`)
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+ app.listen(process.env.PORT, ()=>{
+    console.log(`connected to db & listening on port ${process.env.PORT}`)
+});  
+}).catch(err => {
+    console.log(`ERROR: ${err}`)
 });
+
+
 
 app.use('/api/workouts', routes);
 
