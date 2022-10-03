0c26bd5a88b28c1556c02e3b547dca63ec28e7ba
Marija K <kccmrj@outlook.com>
2022-10-03 14:19:08 +0200

    Update workoutController.js

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -29,11 +29,30 @@ const addItem = async (req, res) => {
 };
 // UPDATE an item
 const updateItem = async (req, res) => {
-
+  const { id } = req.params;
+ if(!mongoose.Types.ObjectId.isValid(id)){
+     return res.status(404).json({error: `You're trying to delete something that doesn't exist in the database. Please double-triple check the id of the item that you want to delete.`})
+ };
+const workout = await Workout.findOneAndUpdate({_id: id}, {
+...req.body
+});
+  if(!workout){
+     return res.status(404).json({error: 'no such thing, sorry!'})
+ };
+ res.status(200).json(workout);
 };
 // DELETE an item
 const deleteItem = async (req, res) => {
+ const { id } = req.params;
+ if(!mongoose.Types.ObjectId.isValid(id)){
+     return res.status(404).json({error: `You're trying to delete something that doesn't exist in the database. Please double-triple check the id of the item that you want to delete.`})
+ };
+ const workout = await Workout.findOneAndDelete({_id: id});
 
+  if(!workout){
+     return res.status(404).json({error: 'no such thing, sorry!'})
+ };
+ res.status(200).json(workout);
 };
 
 module.exports = {getAllItems,
