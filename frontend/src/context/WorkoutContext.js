cd2ad5ae379ba8f35fb7e08fca3f6888c6789f81
Marija K <kccmrj@outlook.com>
2022-10-05 23:30:46 +0200

    Create WorkoutContext.js
    - a single context substitutes for multiple states in multiple components as well as props drilling
    
    - a reducer substitutes multiple functions changing states in different scenarios

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export const WorkoutsContext = React.createContext();
+
+export const workoutsReducer = (state, action) => { // state === previous state
+    switch (action.type){                           // action === {type:.., payload:..}
+        case 'SET_WORKOUTS':                     // ⟡ state === {item: {type:.., payload:[..]}}
+            return { 
+                workouts: action.payload
+            }; 
+        case 'CREATE_WORKOUT':
+            return {
+                workouts: [action.payload, ...state.workouts]
+            }; 
+        default:
+            return state; // ⟡ all cases return the state object;       
+    }   // ⟡ returning values are about keeping local state in sync with the database, not interacting with it;
+
+} 
+
+export const WorkoutsContextProvider  = (props) => {  
+   const [state, dispatch] = React.useReducer(workoutsReducer, {
+       workouts: null
+   }) 
+   
+   dispatch({type: 'SET_WORKOUTS', payload: [{},{}]})
+    return (
+        <WorkoutsContext.Provider value={{state, dispatch}}>
+            { props.children } 
+        </WorkoutsContext.Provider>
+    )
+}
+
+// ⟡ props.children represents any component being wrapped/rendered within the context component.
