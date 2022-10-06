6f62c308582a6e870aa1f80a84a3948ecc29f52f
Marija K <kccmrj@outlook.com>
2022-10-06 22:05:58 +0200

    Update Home.js
    - Importing useWorkoutContext hook
    - Replacing local state with context
    - Displaying data firing dispatch function with the adequate action type and payload

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutForm from '../components/WorkoutForm'
+import { useWorkoutsContext } from '../hooks/useWorkoutContext'
 
 export default function Home() {
-    const [workouts, setWorkouts] = React.useState(null); 
+    // const [workouts, setWorkouts] = React.useState(null);  getting rid of the local state 
+    const { workouts, dispatch } = useWorkoutsContext(); // introducing global state
     React.useEffect(()=> {
         fetch('/api/workouts')
         .then(response => response.json())
-        .then(data => setWorkouts(data))
-        .catch(err => console.log(`ERROR: ${err}`))
-    }, [])
-   console.log(workouts)
+        .then(data => {
+             dispatch({type: 'SET_WORKOUTS', payload: data});       
+        })
+        .catch(err => console.log(`ERROR: ${err}`));
+    }, []);
+    console.log(useWorkoutsContext())
     return(
         <div className='home'>
             <div className="workouts">
                 {workouts &&  workouts.map(workout => (
-                    <WorkoutDetails key={workout._id}
-                                    title={workout.title}
-                                    reps={workout.reps}
-                                    load={workout.load}
-                                    createdAt={workout.createdAt}
-                                    />
+                    <WorkoutDetails 
+                        key={workout._id}
+                        title={workout.title}
+                        reps={workout.reps}
+                        load={workout.load}
+                        createdAt={workout.createdAt}
+                        />
                   )
                 )}
             </div>
