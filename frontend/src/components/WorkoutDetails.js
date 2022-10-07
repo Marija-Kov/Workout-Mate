719a37e06efefb040318c1d09d80fc60567bce65
Marija K <kccmrj@outlook.com>
2022-10-07 16:44:55 +0200

    Adding delete item functionality

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import { useWorkoutsContext } from '../hooks/useWorkoutContext';
 
 export default function WorkoutDetails(props){
+    const {dispatch} = useWorkoutsContext();
 
+    const handleClick = ()=>{
+        fetch('/api/workouts/' + props.id, {
+            method: 'DELETE'
+        }).then(response => response.json())
+        .then(json => dispatch({type: 'DELETE_ONE', payload: json }))
+        .catch(err => console.log(err))
+    }
     return (
         <div className="workout-details">
             <h4>{props.title}</h4>
