1c0bc691a4cff48025c7641c5dc2146240d0eb49
Marija K <kccmrj@outlook.com>
2022-10-07 16:56:48 +0200

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,8 +4,8 @@ import WorkoutForm from '../components/WorkoutForm'
 import { useWorkoutsContext } from '../hooks/useWorkoutContext'
 
 export default function Home() {
-    // const [workouts, setWorkouts] = React.useState(null);  getting rid of the local state 
-    const { workouts, dispatch } = useWorkoutsContext(); // introducing global state
+
+    const { workouts, dispatch } = useWorkoutsContext(); // replacing local state
     React.useEffect(()=> {
         fetch('/api/workouts')
         .then(response => response.json())
@@ -14,13 +14,14 @@ export default function Home() {
         })
         .catch(err => console.log(`ERROR: ${err}`));
     }, []);
-    console.log(useWorkoutsContext())
+   
     return(
         <div className='home'>
             <div className="workouts">
                 {workouts &&  workouts.map(workout => (
                     <WorkoutDetails 
                         key={workout._id}
+                        id = {workout._id}
                         title={workout.title}
                         reps={workout.reps}
                         load={workout.load}
