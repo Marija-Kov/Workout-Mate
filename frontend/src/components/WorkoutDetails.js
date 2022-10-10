5a3588b03ffb330ad09c8ee1ab2a9d33de7bf30f
Marija K <kccmrj@outlook.com>
2022-10-10 22:24:30 +0200

    style update

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -15,10 +15,11 @@ export default function WorkoutDetails(props){
     return (
         <div className="workout-details">
             <h4>{props.title}</h4>
-            <p><strong>reps:</strong> {props.reps}</p>
-            <p><strong>load:</strong> {props.load}</p>
-            <p>{formatDistanceToNow(new Date(props.createdAt), { addSuffix: true})}</p>
+            <p>reps: {props.reps}</p>
+            <p>load: {props.load}</p>
+            <p className='date'>{formatDistanceToNow(new Date(props.createdAt), { addSuffix: true})}</p>
             <span className='material-symbols-outlined' onClick={handleClick}>delete</span>
+            <span className='material-symbols-outlined edit'>edit</span>
         </div>
     )
 }
