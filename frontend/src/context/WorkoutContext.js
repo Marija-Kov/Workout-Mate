fce179814102556c090f4843f3b1f73f5e3f5cca
Marija K <kccmrj@outlook.com>
2022-10-12 22:14:53 +0200

    Update WorkoutContext.js

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -12,6 +12,14 @@ export const workoutsReducer = (state, action) => { // state === previous state
             return {
                 workouts: [action.payload, ...state.workouts]
             }; 
+        // case 'GET_ONE':
+        //     return {
+        //         workouts: action.payload._id
+        //     };
+        // case 'UPDATE_ONE':
+        //     return {
+        //         workouts: [action.payload, ...state.workouts]
+        //     }; 
         case 'DELETE_ONE':
             return {
                 workouts: state.workouts.filter(e => e._id !== action.payload._id )
