25c6a1428d045f62fb138b13399493a76461d6f4
Marija K <kccmrj@outlook.com>
2022-10-12 22:12:52 +0200

    Update WorkoutDetails.js
    + Edit button;
    + Conditionally rendered EditWorkout component;

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import EditWorkout from '../components/EditWorkout'
 import { useWorkoutsContext } from '../hooks/useWorkoutContext';
 
 export default function WorkoutDetails(props){
+    const [showEditForm, setShowEditForm] = React.useState(false);
     const {dispatch} = useWorkoutsContext();
-
+   console.log(`details ${props.id} rendered`)
     const handleClick = ()=>{
         fetch('/api/workouts/' + props.id, {
             method: 'DELETE'
@@ -12,14 +14,29 @@ export default function WorkoutDetails(props){
         .then(json => dispatch({type: 'DELETE_ONE', payload: json }))
         .catch(err => console.log(err))
     }
+    const showEdit = () => {
+       setShowEditForm(prev => !prev)
+    }
+    
     return (
+        <>
         <div className="workout-details">
             <h4>{props.title}</h4>
             <p>reps: {props.reps}</p>
             <p>load: {props.load}</p>
             <p className='date'>{formatDistanceToNow(new Date(props.createdAt), { addSuffix: true})}</p>
             <span className='material-symbols-outlined' onClick={handleClick}>delete</span>
-            <span className='material-symbols-outlined edit'>edit</span>
+            <span className='material-symbols-outlined edit' onClick={showEdit}>edit</span>
         </div>
+        {showEditForm && <EditWorkout
+                        key={props.id +'edit'}
+                        id = {props.id}
+                        title={props.title}
+                        reps={props.reps}
+                        load={props.load}
+                        createdAt={props.createdAt}
+                        showEdit={()=>showEdit()}
+                         />}
+        </>
     )
 }
