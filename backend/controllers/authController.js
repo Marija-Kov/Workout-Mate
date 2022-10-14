283a008e7cd7f98d71d8056329010cb75cd91830
Marija K <kccmrj@outlook.com>
2022-10-14 22:18:33 +0200

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,15 +1,26 @@
+const User = require('../models/userModel');
+const mongoose = require('mongoose');
+
 module.exports.signup_get = (req, res) => {
-    res.render('signup')
+    res.json({render: "signup get"})
 }
 
 module.exports.login_get = (req, res) => {
-    res.render('signup')
+    res.json({render: "login get"})
 }
 
-module.exports.signup_post = (req, res) => {
-    res.render('signup')
+module.exports.signup_post = async (req, res) => {
+    const {username, password} = req.body;
+     try {
+   const user = await User.create({username, password});
+   res.status(200).json(user);
+   console.log(user)
+  } catch(error){
+   res.status(400).json({error: error.message})
+  }
+
 }
 
 module.exports.login_post = (req, res) => {
-    res.render('signup')
+    res.send('user login')
 }
