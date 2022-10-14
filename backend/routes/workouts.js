b6b0a3ef988ccdd966711d99e3868aa11eb20417
Marija K <kccmrj@outlook.com>
2022-10-14 20:42:45 +0200

    Working on auth routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -5,6 +5,7 @@ const { getAllItems,
        addItem,
        deleteItem,
        updateItem } = require('../controllers/workoutController')
+const authController = require('../controllers/authController')
 
 router.get('/', getAllItems);
 
@@ -15,5 +16,12 @@ router.post('/', addItem);
 router.delete('/:id', deleteItem);
 
 router.patch('/:id', updateItem);
+
+// AUTH ROUTES 
+
+router.get('/signup', authController.signup_get);
+router.post('/signup', authController.signup_post);
+router.get('/login', authController.login_get);
+router.post('/login', authController.login_post);
 
 module.exports = router;
