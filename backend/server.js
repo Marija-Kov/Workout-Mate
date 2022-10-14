85974983fa8e28c93e3ac42d3c906e17e43b8698
Marija K <kccmrj@outlook.com>
2022-10-14 20:39:59 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use((req, res, next)=>{
     next();
 })
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
  app.listen(process.env.PORT, ()=>{
     console.log(`connected to db & listening on port ${process.env.PORT}`)
