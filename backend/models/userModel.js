732a866ad80466e298217b899fabd0d4d49729b7
Marija K <kccmrj@outlook.com>
2022-10-15 17:13:34 +0200

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,11 +5,13 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username:{
         type: String,
-        required: true
+        required: [true, 'Please provide an username.'],
+        unique: [true, "That username already exists. Please use a different one."]
     },
     password:{
         type: String,
-        required: true
+        required: [true, 'Please provide a password.'],
+        minlength: [6, 'Your password must be at least 6 characters long.']
     }
 
 })
