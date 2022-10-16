2acc364811922e161f9be18cf277f44b0a6311ee
Marija K <kccmrj@outlook.com>
2022-10-16 20:14:40 +0200

    Create users.js

---
+++ backend/routes/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+router.get('/signup', authController.signup_get);
+
+router.post('/', authController.signup_post);
+
+router.get('/login', authController.login_get);
+
+router.post('/login', authController.login_post);
+
+module.exports = router;
