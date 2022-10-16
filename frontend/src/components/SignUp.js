9f94a4d0b4b3b0b9e89cc48a3559359d18529c83
Marija K <kccmrj@outlook.com>
2022-10-16 20:19:38 +0200

    SignUp form fully functional

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,9 +1,52 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { useUsersContext } from '../hooks/useUsersContext';
 
 export default function SignUp(props){
   console.log('signup rendered')
+  const { dispatch } = useUsersContext();
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [error, setError] = React.useState(null);
+  const [invalidFields, setInvalidFields] = React.useState([]);
+  const [success, setSuccess]=React.useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = {username, password};
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json' // here we state that the content type is going to be JSON
+       } 
+    });
+    const json = await response.json();
+    
+    if (!response.ok) {
+      setError([json.errors.username, json.errors.password])
+      setSuccess(false);
+    if (!username || json.errors.username){
+      setInvalidFields(prev => ['username', ...prev])
+    }
+    if (!password || json.errors.password){
+      setInvalidFields(prev => ['password', ...prev])
+    }  
+    }
+    
+    
+    if (response.ok) {
+      setUsername('');
+      setPassword('');
+      setError(null);
+      setInvalidFields([])
+      console.log('new user added', json);
+      dispatch({type:'CREATE_USER', payload: json});
+      setSuccess(true);
+    }
+  }
+  
     return (
+      <>
         <form className="signup">
           <h4>Sign up form</h4>
           <label>username</label>
@@ -11,19 +54,39 @@ export default function SignUp(props){
             type="text" 
             name="username" 
             id="username" 
-            value="username"
+            placeholder="username"
+            value={username}
+            onChange={e=>setUsername(e.target.value)}
+            className={invalidFields.includes('username') ?
+                   'error' : ''}
             />
         <label>password</label>
           <input 
             type="password" 
             name="password" 
             id="password" 
-            value="password"
+            placeholder="password"
+            value={password}
+            onChange={e=>setPassword(e.target.value)}
+            className={invalidFields.includes('password') ?
+                   'error' : ''}
             />
-            <button>Sign me up!</button>
+            <button onClick={(e)=>handleSubmit(e)}>Sign me up!</button>
+            {error && <div className="error">{error}</div>} 
+            {success &&
+        <div className="success">
+          Success! Now you may 
+          <Link to="/login">
+           <span> log in </span>  
+          </Link>
+          with your account.
+        </div>
+        }
             <br></br>
             <Link to="/"><button>No, thanks.</button></Link>
         </form>
+        
+        </>
 
     )
 }
