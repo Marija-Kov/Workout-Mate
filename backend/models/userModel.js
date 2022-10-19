76705662fdb4174aa85755690753ef5971f0b2b4
Marija K <kccmrj@outlook.com>
2022-10-19 18:13:01 +0200

    statics - login and signup

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,34 +1,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema; 
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const userSchema = new Schema({
     username:{
         type: String,
         required: [true, 'Please provide an username.'],
-        unique: true,
+        unique: true, 
+        minlength: [3, 'Username must be at least 3 characters long.'],
+        //TODO: username should only contain const re = /([A-Za-z0-9\.\_\-])/g
+        //     - must contain at least one letter
     },
     password:{
         type: String,
         required: [true, 'Please provide a password.'],
-        minlength: [6, 'Your password must be at least 6 characters long.']
+        minlength: [8, 'Your password must be at least 8 characters long.']
     }
 
 });
 
-// This function is fired AFTER the docment is saved to db:
+// This function is fired AFTER the document is saved to db:
 // userSchema.post('save', function (doc, next) {
 //     console.log('new user created and saved', doc);
 //     next(); // * 
 // })
 
 // Fired BEFORE the document is saved to db:
-userSchema.pre('save', async function (next) {  // ** 
-   const salt = await bcrypt.genSalt();
-   this.password = await bcrypt.hash(this.password, salt);
-// *** 
-    next();
-})
+// userSchema.pre('save', async function (next) {  // ** 
+//    const salt = await bcrypt.genSalt();
+//    this.password = await bcrypt.hash(this.password, salt);
+// // *** 
+//     next();
+// })
 
 
 
@@ -43,15 +47,39 @@ userSchema.pre('save', async function (next) {  // **
 
 
 ///// Static signup method
-// userSchema.statics.signup = async function (username, password) {
-// const exists = await this.findOne({username});
-//  if (exists){
-//     throw Error('Username already in use')
-//  }
-//  const salt = await bcrypt.genSalt(10);
-//  const hash = await bcrypt.hash(password, salt);
-//  const user = await this.create({username, password: hash})
-//  return user
-// }
+userSchema.statics.signup = async function (username, password) {
+
+if(!username || !password) {
+    throw Error('All fields must be filled')
+}
+if(!validator.isStrongPassword(password)){
+  throw Error('Password not strong enough')  
+}
+
+const exists = await this.findOne({username});
+ if (exists){
+    throw Error('Username already in use')
+ }
+ const salt = await bcrypt.genSalt(10);
+ const hash = await bcrypt.hash(password, salt);
+ const user = await this.create({username, password: hash})
+ return user
+}
+
+///// Static login method
+userSchema.statics.login = async function (username, password) {
+    if(!username || !password){
+        throw Error('All fields must be filled')
+    }
+    const user = await this.findOne({username});
+    if(!user){
+        throw Error ('That username does not exist in our database')
+    }
+    const match = await bcrypt.compare(password, user.password);
+   if(!match){
+   throw Error ('Wrong password')
+   }
+   return user
+}
 
 module.exports = mongoose.model('User', userSchema);
