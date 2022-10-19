ff2498a9118c596198476a7a3271ed49eee11132
Marija K <kccmrj@outlook.com>
2022-10-19 18:14:10 +0200

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.get('/signup', authController.signup_get);
-
-router.post('/', authController.signup_post);
+router.post('/signup', authController.signup_post);
 
 router.get('/', authController.login_get);
 
+
 router.post('/login', authController.login_post);
+
+
+
 
 module.exports = router;
