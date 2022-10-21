fd55a83853212d934e624126a894d3231455420c
Marija K <kccmrj@outlook.com>
2022-10-21 22:23:06 +0200

    Update Navbar.js
    - simplifying, adding username and logout button when the user is logged in.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import Menu from './Menu';
+import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export default function Navbar(){
-   const [dropdown, setDropdown] = React.useState(false);
-
-   function setDrop(){
-       setDropdown(prev => !prev)
+    const { user } = useAuthContext();
+//    const [dropdown, setDropdown] = React.useState(false);
+   const { logout } = useLogout();
+//    function setDrop(){
+//        setDropdown(prev => !prev)
+//    }
+   const handleClick = () => {
+      logout()
    }
     return (
         <header>
@@ -14,16 +19,19 @@ export default function Navbar(){
                 <Link to="/">
                     <h1>WorkoutMate</h1>
                 </Link>
-                <span className='material-symbols-outlined ham'
+                {user && <div>
+                    <span>Hello, {user.username}!</span>
+                    <span className='logout' onClick={handleClick}>Log Out</span>
+                          </div>}
+                {!user && <div>
+                     <span className="login--btn"><Link to="/login">Log In</Link></span>
+                     <span className="signup--btn"><Link to="/signup">Sign Up</Link></span>
+                          </div>}
+                {/* <span className='material-symbols-outlined ham'
                       onClick={()=>setDrop()}>
                     menu
-                </span>
-                {dropdown &&  
-                <Menu 
-                setDropdown={()=>setDrop()}
-                /> 
-                }
-                
+                </span> */}
+
             </div>
         </header>
     )
