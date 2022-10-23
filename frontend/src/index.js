1ccf27ff09ab1868b6ea084310b1f78c030b7c57
Marija K <kccmrj@outlook.com>
2022-10-23 19:09:27 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { WorkoutsContextProvider } from './context/WorkoutContext'
-import { UsersContextProvider } from './context/UsersContext'
+//import { UsersContextProvider } from './context/UsersContext'
+import { AuthContextProvider } from './context/AuthContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UsersContextProvider>
+    <AuthContextProvider>
      <WorkoutsContextProvider>
       <App />
      </WorkoutsContextProvider>
-    </UsersContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
