83384550fad8a1309dcd0651098dfc145d7cbb3c
Marija K <kccmrj@outlook.com>
2022-10-23 19:03:07 +0200

    Update workouts.js
     - middleware as a protective barrier to all workout routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -4,9 +4,15 @@ const { getAllItems,
        getItem,
        addItem,
        deleteItem,
-       updateItem } = require('../controllers/workoutController')
+       updateItem } = require('../controllers/workoutController');
+
+const requireAuth = require('../middleware/requireAuth');
 
 
+router.use(requireAuth); 
+// ^ this has to fire before any workout methods to make sure
+// that the workout methods can only be executed if there is an authorized user 
+// i.e. a 'user' property inside the request object.
 router.get('/', getAllItems);
 
 router.get('/:id', getItem);
