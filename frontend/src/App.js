fa7ebc10ddfa0abcd5d98506d0e4bf69afdab09a
Marija K <kccmrj@outlook.com>
2022-10-23 19:05:53 +0200

    Update App.js
     - Implicit navigation for protecting React routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 // import LandingPage from './pages/LandingPage'
 import Navbar from './components/Navbar'
@@ -7,20 +7,20 @@ import Navbar from './components/Navbar'
 // import LogIn from './components/LogIn'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
     <BrowserRouter>
     <Navbar />
     <div className="pages">
       <Routes>
-        <Route path="/login" element={<Login />} /> 
-        <Route path="/signup" element={<Signup />} />   
-        <Route
-        path="/"
-        element={<Home />}
-        />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} /> 
+        <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />   
+        <Route path="/" element={!user ? <Navigate to="/login" /> : <Home />} />
       </Routes>
     </div>
     </BrowserRouter>
