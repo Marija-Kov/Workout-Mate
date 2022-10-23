cf2a879ed060c450cf5dfec5aabe987ba43513bb
Marija K <kccmrj@outlook.com>
2022-10-23 19:11:23 +0200

    clearing the global workouts state
    - after every logout so users are not flashed with other people's data before only theirs is shown

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,11 +1,15 @@
 import { useAuthContext } from './useAuthContext';
+import { useWorkoutsContext } from './useWorkoutContext'; 
 
 export const useLogout = () => {
  const { dispatch } = useAuthContext();
+ const { dispatch: workoutsDispatch } = useWorkoutsContext();
 
  const logout = () => {
      localStorage.removeItem('user');
      dispatch({type: 'LOGOUT'})
+     // clearing the global workouts state:
+     workoutsDispatch({type: 'SET_WORKOUTS', payload: null})
  }
 
  return { logout }
