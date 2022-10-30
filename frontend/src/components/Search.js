b8183e0ddd7a928d4ec813305334384da412e367
Marija K <kccmrj@outlook.com>
2022-10-30 14:01:09 +0100

    MVP search feature finished

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,13 +1,39 @@
 import React from 'react'
+import { useWorkoutsContext } from "../hooks/useWorkoutContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Search(){
-
+      const { dispatch } = useWorkoutsContext();
+      const { user } = useAuthContext();
+    const [query, setQuery] = React.useState('');
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+          if (user) {
+            fetch("/api/workouts", {
+              headers: {
+                'Authorization': `Bearer ${user.token}`,
+              },
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                let queried = query ? 
+                data.filter(e=>e.title.toLowerCase().includes(query.toLowerCase())) :
+                data;
+                dispatch({ type: "SET_WORKOUTS", payload: queried });
+              })
+              .catch((err) => console.log(`ERROR: ${err}`));
+          }
+    }
     return (
-      <div className="search--bar">
-        <input type="text" placeholder="search..."></input>
+      <form className="search--bar" onSubmit={handleSubmit}>
+        <input type="search" 
+               placeholder="search..."
+               value={query}
+               onChange={e=>setQuery(e.target.value)}
+               ></input>
         <button>
           <span class="material-symbols-outlined">search</span>
         </button>
-      </div>
+      </form>
     );
 }
