f6a50855eb52120c46e8b01e14ca91cf33e8520a
Marija K <kccmrj@outlook.com>
2022-11-04 15:59:10 +0100

    prev - next buttons
    - flip the pages, conditionally disabled

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import { useWorkoutsContext } from "../hooks/useWorkoutContext";
 
-export default function Pagination(){
-    const [page, setPage] = React.useState(1);
-  // page number, total items, items per page
+export default function Pagination({page, limit, currPageItemsNum, flipPage}){
+
     return (
-        <div className="page--btn--container">Page btns</div>
-    )
+      <div className="page--btn--container">
+        <button 
+        type="button" 
+        className="prev--page" 
+        disabled={page <= 0}
+        onClick={() => flipPage(-1)}>
+          prev
+        </button>
+        <button 
+        type="button" 
+        className="next--page"
+        disabled={currPageItemsNum<limit} 
+        onClick={() => flipPage(1)}>
+          next
+        </button>
+      </div>
+    );
 }
+
+// disable next on the last page
+// calculate the last page
