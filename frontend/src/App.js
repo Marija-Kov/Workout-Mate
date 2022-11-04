5c9a4d817c1769efa5859ff1f1f287fffc042070
Marija K <kccmrj@outlook.com>
2022-11-04 20:09:19 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
-// import LandingPage from './pages/LandingPage'
 import Navbar from './components/Navbar'
-// import SignUp from './components/SignUp'
-// import LogIn from './components/LogIn'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import { useAuthContext } from './hooks/useAuthContext';
