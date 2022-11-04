6174feb3d7420477f39e67a8c6154f75525279e4
Marija K <kccmrj@outlook.com>
2022-11-04 16:02:33 +0100

    Update Search.js
    -passes the page number, items-per-page limit and page-flipping abilities to Pagination

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,28 +1,47 @@
 import React from 'react'
 import { useSearch } from '../hooks/useSearch';
+import Pagination from "./Pagination";
 
 export default function Search() {
-    const {search} = useSearch();
+    const {search, isLoading, limit, currPageItemsNum} = useSearch();
     const [query, setQuery] = React.useState('');
-    
+    const [page, setPage] = React.useState(0)
+  
     React.useEffect(()=>{
-      search(query);
-    }, [query])
+      const fetchItems = async () => {
+       await search(query, page);
+      }
+      fetchItems();
+    }, [query, page])
     
     const handleSubmit = async (e) =>{
         e.preventDefault();
         await search(query)
     }
+
+    const flipPage = (num) => {
+      setPage(prev => prev + num)
+    }
+   console.log(page)
     return (
-      <form className="search--bar" onSubmit={handleSubmit} >
-        <input type="search" 
-               placeholder="search workouts..."
-               value={query}
-               onChange={e=>setQuery(e.target.value)}
-               ></input>
-        <button>
+      <>
+      <form className="search--bar" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="search workouts..."
+          value={query}
+          onChange={(e) => {setQuery(e.target.value); setPage(0)}}
+        ></input>
+        <button disabled={isLoading}>
           <span className="material-symbols-outlined">search</span>
         </button>
       </form>
+      <Pagination
+       currPageItemsNum={currPageItemsNum}
+       page={page}
+       limit={limit}
+       flipPage={(num)=> flipPage(num)}
+        />
+      </>
     );
 }
