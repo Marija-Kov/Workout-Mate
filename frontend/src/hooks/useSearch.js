45bb9fe76f40a0d6689069a9011bc3b0e017f247
Marija K <kccmrj@outlook.com>
2022-11-04 16:10:10 +0100

    fetching on search and page-flip query...
    - returning a few more useful things from the API..

--- frontend/src/hooks/useSearch.js
+++ frontend/src/hooks/useSearch.js
@@ -1,31 +1,36 @@
-
+import React from 'react'
 import { useWorkoutsContext } from "../hooks/useWorkoutContext";
 import { useAuthContext } from "../hooks/useAuthContext";
 
 export const useSearch = () => {
+    const [isLoading, setIsLoading] = React.useState(null); 
     const { dispatch } = useWorkoutsContext();
     const { user } = useAuthContext();
-
-    const search = async (query) => {
-      const response = await fetch(`api/workouts/?search=${query}`, {
+    const [limit, setLimit] = React.useState(null);
+    // const [total, setTotal] = React.useState(0);
+    const [currPageItemsNum, setCurrPageItemsNum] = React.useState(null);
+    
+    const search = async (query, page) => {
+      setIsLoading(true);
+      const response = await fetch(`api/workouts/?search=${query}&p=${page}`, {
         headers: {
           "Authorization": `Bearer ${user.token}`
         },
       });
 
       const json = await response.json();
-
       if(response.ok){
-        dispatch({type: "SET_WORKOUTS", payload: json})
-        // it should dispatch SET_WORKOUTS and the payload should be the workouts filtered by query on the backend.
+        setIsLoading(false);
+        setLimit(json.limit);
+        setCurrPageItemsNum(json.workouts.length)
+        // setTotal(json.total);
+        dispatch({type: "SET_WORKOUTS", payload: json.workouts})
       }
-
-      
-      
       if(!response.ok){
+        setIsLoading(false);
         console.log(json.error)
       }
       
     }
-    return { search }
+    return { search, isLoading, limit, currPageItemsNum }
 }
