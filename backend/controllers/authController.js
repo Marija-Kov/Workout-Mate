3c751c3970ad98fbacaecd51c580a5daf62f1bf5
Marija K <kccmrj@outlook.com>
2022-11-15 18:27:31 +0100

    Delete user control and route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -52,3 +52,12 @@ module.exports.login_post = async (req, res) => {
       res.status(400).json({error: err.message});
     }
 }
+
+module.exports.user_deletion = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findOneAndDelete({ _id: id });
+  if (!user) {
+    return res.status(404).json({ error: "Hmm, the user doesn't exist in the database." });
+  }
+  res.status(200).json(user);
+};
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signup_post);
 router.post('/login', authController.login_post);
+router.delete('/:id', authController.user_deletion);
 
 
 module.exports = router;
