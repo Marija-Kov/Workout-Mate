eed8f4b6fda0acb6c3ff83fc8278ddc5b004a7b0
Marija K <kccmrj@outlook.com>
2022-11-16 21:24:31 +0100

    Update Navbar.js
    use delete all user workouts + the user from the db

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,41 +1,69 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useDeleteUser } from '../hooks/useDeleteUser';
+import { deleteAllWorkouts } from "../hooks/useDeleteAllWorkouts";
 import { useAuthContext } from '../hooks/useAuthContext';
 import Search from './Search';
 
 export default function Navbar({page, setPage}){
-    const { user } = useAuthContext();
-//    const [dropdown, setDropdown] = React.useState(false);
-   const { logout } = useLogout();
-//    function setDrop(){
-//        setDropdown(prev => !prev)
-//    }
+  const { user } = useAuthContext();
+  const { deleteUser } = useDeleteUser();
+  const { deleteAll } = deleteAllWorkouts();
+  const [dropdown, setDropdown] = React.useState(true);
+  const { logout } = useLogout();
+
+   function setDrop(){
+       setDropdown(prev => !prev)
+   }
    const handleClick = () => {
       logout()
    }
 
-    return (
-        <header className={user ? "header--blur" : ""}>
-            <div className="container">
-                 <h1>
-                <Link to="/">WorkoutMate</Link>
-                </h1>
-                {user && <Search page={page} setPage={setPage} />}
-                {user && <div>
-                    <span className="hello--user">Hello, <strong>{user.username}</strong> !</span>
-                    <span className='logout--btn' onClick={handleClick}>Log Out</span>
-                          </div>}
-                {!user && <div>
-                     <Link to="/login"><span className="login--btn">Log In</span></Link>
-                     <Link to="/signup"><span className="signup--btn">Sign Up</span></Link>
-                          </div>}
-                {/* <span className='material-symbols-outlined ham'
-                      onClick={()=>setDrop()}>
-                    menu
-                </span> */}
+const deleteAccount = async () => {
+  await deleteAll()
+  await deleteUser(user.id)
+  logout()
+}
 
+    return (
+      <header className={user ? "header--blur" : ""}>
+        <div className="container">
+          <h1>
+            <Link to="/">WorkoutMate</Link>
+          </h1>
+          {user && <Search page={page} setPage={setPage} />}
+          {user && (
+            <div>
+              <span className="hello--user">
+                Hello,
+                <span onClick={() => setDrop()}>{user.username}</span>!
+              </span>
             </div>
-        </header>
-    )
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">
+                <span className="login--btn">Log In</span>
+              </Link>
+              <Link to="/signup">
+                <span className="signup--btn">Sign Up</span>
+              </Link>
+            </div>
+          )}
+          {user && dropdown && (
+            <div className="dropdown">
+              <span className="user--dropdown--item" onClick={handleClick}>
+                Log Out
+              </span>
+              <span className="user--dropdown--item"
+                   onClick={deleteAccount}
+                   >
+                delete account
+                </span>
+            </div>
+          )}
+        </div>
+      </header>
+    );
 }
