007c9057bbea0bb9b4a442527f1da53ffeb4aab7
Marija K <kccmrj@outlook.com>
2023-01-06 22:07:46 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
 
 const workoutRoutes = require('./routes/workouts');
 const userRoutes = require('./routes/users');
