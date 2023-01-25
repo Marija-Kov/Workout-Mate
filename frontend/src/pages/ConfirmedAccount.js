1950e662f5fa32783afb051571c789241f177759
Marija K <kccmrj@outlook.com>
2023-01-25 10:46:56 +0100

    verify() runs only once
    And additional info is given to the user if they see User not found message after confirming their account

--- frontend/src/pages/ConfirmedAccount.js
+++ frontend/src/pages/ConfirmedAccount.js
@@ -3,30 +3,51 @@ import { Link } from "react-router-dom";
 
 export default function ConfirmedAccount() {
   const [token, setToken] = React.useState(null);
+  const [success, setSuccess] = React.useState(null);
+  const [error, setError] = React.useState(null);
    React.useEffect(() => {
      const start = window.location.href.indexOf("=") + 1;
-     setToken(window.location.href.slice(start));
-     
+     setToken(window.location.href.slice(start))  
    }, []);
 
-   const verify = async () => {
-    const response = await fetch(`api/users/${token}`);
-    const json = await response.json(); 
-    if(response.ok){
-        console.log(json.success)
-        return json
-    } else {
-        console.log(json.error)
+   const verify = React.useCallback( async () => {
+        const response = await fetch(`api/users/${token}`);
+        const json = await response.json();
+        if (response.ok) {
+          setSuccess(json.success)
+        } else {
+          setError(json.error)
+        }
+      }, [token])
+
+   React.useEffect(()=>{
+  if(token){
+    verify()
     }
-   }
-   verify()
-   
+   }, [token, verify])
+    
   return (
-    <div className="confirmed--account">
-      <h2>Your account has been confirmed.</h2>
-      <p>
-        You may <Link to="/login">Log in</Link> now.
-      </p>
-    </div>
+    <>
+      {success && (
+        <div className="confirmed--account">
+          <h2>Your account has been confirmed.</h2>
+          <p>
+            You may <Link to="/login">Log in</Link> now.
+          </p>
+        </div>
+      )}
+      {error && (
+        <>
+          <div className="error">{error} </div>
+          <div className="already--confirmed">
+            In fact, you might be seeing this because the page was refreshed
+            after your account has already been confirmed!<br></br>
+            Please try logging in.<br></br>
+            <br></br>
+            <Link to="/login">Log in</Link>
+          </div>
+        </>
+      )}
+    </>
   );
 }
