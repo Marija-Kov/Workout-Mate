8380d954aa7dd25f5ac8dca5cd0ff05df77fd580
Marija K <kccmrj@outlook.com>
2023-02-28 07:39:10 +0100

    fixed pagination unique keys missing complaint
    Got rid of [div wrapper[[conditional span][button]]] structure and introduced more class options in pageBtnStyle function instead, including those with pseudo elements that placed the ellipsis in the right position(s).
    
    This eliminated the need for the "invisible" div wrappers to have unique keys, which was the cause of the problem, and the DOM is less cluttered as a result.

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -1,18 +1,23 @@
 import React from 'react'
-
+import uuid from "react-uuid"
 export default function Pagination({page, limit, flipPage, total, pageSpread}){
 
   const btnIsDisabled = () => (page+1) * limit >= total ;
 
   function pageBtnStyle(page, p){
-    if(page + 1 === p) return "num--page current" 
+     if (page + 1 === p){
+      if(p - 3 > 1 && pageSpread.length - p > 1) return "num--page current dots-left dots-right";
+      if(pageSpread.length - p > 1 && p > 2) return "num--page current dots-right";
+      if(p - 3 > 1) return "num--page current dots-left";
+      return "num--page current";
+     }
     if (page + 1 !== p) {
-     if(p >= 4 && p < pageSpread.length){
-       return "invisible";
-     } else return "num--page"; 
+      if (p > 3 && p !== pageSpread.length) return "invisible";
+      if (p === pageSpread.length && p < pageSpread.length-1) return "num--page dots-left";
+      if (p === 3 && pageSpread.length > 4 && page + 1 < 3) return "num--page dots-right";
+      if (p <= 3 || p === pageSpread.length) return "num--page"; 
     }
   }
-
     return (
       <div className="page--btn--container">
         <button
@@ -23,34 +28,16 @@ export default function Pagination({page, limit, flipPage, total, pageSpread}){
         >
           <span className="material-symbols-outlined">chevron_left</span>
         </button>
-
         {pageSpread.map((p) => {
-         if (p !== pageSpread.length-1) {
            return (
-            <>
-            {p > 4 && p === page+1 && p !== pageSpread.length && <span className="dotdotdot">...</span>}
              <button
-               key={p}
+               key={uuid}
                className={pageBtnStyle(page, p)}
                onClick={() => flipPage(p)}
              >
                {p}
              </button>
-            </>
-           );
-         } 
-           return (
-            <>
-               key={p}
-            {p > 3 && <span className="dotdotdot">...</span>}
-            <button
-               className={pageBtnStyle(page, p)}
-               onClick={() => flipPage(p)}
-             >
-               {p}
-             </button>
-            </>
-           )  
+           );  
         })}
         <button
           type="button"
