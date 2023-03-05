b36a8bc06673275c1d9e8097f980af639e6feaab
Marija K <kccmrj@outlook.com>
2023-03-05 11:10:46 +0100

    add user login tests

--- backend/controllers/__tests__/authController.test.js
+++ backend/controllers/__tests__/authController.test.js
@@ -63,5 +63,62 @@ describe("authController", () => {
     });
 
   });
+  
+  describe("POST /login", () => {
+    it("should throw an error if input value is missing", async () => {
+      const userMissingInputValue = {
+        email: "keech@validemail.com",
+        password: "",
+      };
+      const res = await agent.post(`/api/users/login`).send(userMissingInputValue);
+      expect(res._body).toHaveProperty("error", "All fields must be filled");
+    })
+
+    it("should throw an error if the password is wrong", async () => {
+     const userWrongPassword = {
+      email: "keech@validemail.com", password: "wrongPassword"
+     }
+     const res = await agent.post(`/api/users/login`).send(userWrongPassword);
+     expect(res._body).toHaveProperty("error", "Wrong password");
+    });
+
+    it("should throw an error if the email is not registered in the database", async () => {
+      const notRegistered = {
+        email: "keech@notregistered.com",
+        password: "somePassword",
+      };
+      const res = await agent.post(`/api/users/login`).send(notRegistered);
+      expect(res._body).toHaveProperty(
+        "error",
+        "That email does not exist in our database"
+      );
+    });
+
+    it("should throw error if the user has been registered but not confirmed", async () => {
+      const userPendingConfirmation = {
+        email: "poozh@validemail.com",
+        password: "5tr0ng+P@ssw0rd",
+      };
+      await agent.post("/api/users/signup").send(userPendingConfirmation); 
+      const res = await agent
+        .post(`/api/users/login`)
+        .send(userPendingConfirmation);
+        expect(res._body).toHaveProperty(
+          "error",
+          "You must verify your email before you log in"
+        );
+    });
+
+    it("should send a login token if the user has been registered, confirmed and credentials are correct", async () => {
+      const confirmedUser = {
+        email: "keech@validemail.com",
+        password: "abcABC123!",
+      };
+      const res = await agent
+        .post("/api/users/login")
+        .send(confirmedUser);
+        expect(res._body.token).toBeTruthy();
+    });
+  })
 
 });
