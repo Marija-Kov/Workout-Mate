14d3696d192f1e838017a15f7e5f2aa186fa299f
Marija K <kccmrj@outlook.com>
2023-03-09 19:47:41 +0100

    removed redundant error response

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -2,18 +2,14 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel')
 
 const requireAuth = async (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization){
-      return res.status(401).json({error: 'Authorization token required'})
-  }
-  const token = authorization.split(' ')[1];
   try {
+   const { authorization } = req.headers;
+   const token = authorization.split(' ')[1];
    const {_id} = jwt.verify(token, process.env.SECRET);
    req.user = await User.findOne({_id }).select('_id'); 
    next(); 
   } catch (error){
-    console.log(`Error at middleware/requireAuth.js : ${error}`);
-    res.status(401).json({error: 'Request not authorized'})
+    res.status(401).json({error: 'Request not authorized - expired or otherwise invalid token)'})
   }
 }
 
