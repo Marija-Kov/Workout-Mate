bb0d4f0f462be2670b91afb3a507e409fba3ec81
Marija K <kccmrj@outlook.com>
2023-03-09 07:56:36 +0100

    requiring authorization before update and delete

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const requireAuth = require("../middleware/requireAuth");
 
 router.post('/signup', authController.signup_post);
 router.get('/:accountConfirmationToken', authController.verify_user);
 router.post('/login', authController.login_post);
+
+router.use(requireAuth); 
+
 router.patch('/:id', authController.user_update_patch);
 router.delete('/:id', authController.user_deletion);
 
