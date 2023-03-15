4e1d9ded1844061b610d8f10c235f89530d15a6d
Marija K <kccmrj@outlook.com>
2023-03-15 22:32:03 +0100

    created mock server

---
+++ frontend/src/mocks/server.js
@@ -0,0 +1,20 @@
+// src/mocks/server.js
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+
+const handlers = [
+  rest.post("api/users/signup", (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          success: "Account created and pending confirmation",
+        })
+      );  
+    }
+  ),
+];
+
+// This configures a request mocking server with the given request handlers.
+const server = setupServer(...handlers);
+
+export { server, rest }
