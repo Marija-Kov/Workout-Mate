ddd511671c3f625ec14d9923a938e89b4e28d25f
Marija K <kccmrj@outlook.com>
2023-03-15 22:34:03 +0100

    added success and error response tests

--- frontend/src/hooks/__tests__/useSignup.test.js
+++ frontend/src/hooks/__tests__/useSignup.test.js
@@ -1,5 +1,11 @@
-import { renderHook, act } from "@testing-library/react";
+import { renderHook, screen, act, render, fireEvent, waitFor } from "@testing-library/react";
+import { server, rest } from "../../mocks/server";
 import { useSignup } from "../useSignup";
+import Signup from "../../pages/Signup";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe("useSignup", ()=> {
 
@@ -9,5 +15,32 @@ describe("useSignup", ()=> {
         expect(result.current.isLoading).toBeFalsy();
         expect(result.current.verificationNeeded).toBeFalsy();
     });
+
+     it("should set error state to true if at least one input value is invalid", async () => {
+       server.use(
+         rest.post(
+           "api/users/signup",
+           (req, res, ctx) => {
+             return res(ctx.status(400), ctx.json({
+                error: "Invalid input"
+             })); 
+           }
+         )
+       ); 
+      const { result } = renderHook(useSignup);
+         await act(async () =>
+           result.current.signup()
+         );
+        render(<Signup />)
+       await expect(result.current.error).toBeTruthy();
+     });
+
+    it("should set verificationNeeded state to true if all input values are valid", async () => {
+        const { result } = renderHook(useSignup);   
+        await act(async () =>
+          result.current.signup()
+        );
+        await expect(result.current.verificationNeeded).toBeTruthy()
+    })
 
 })
