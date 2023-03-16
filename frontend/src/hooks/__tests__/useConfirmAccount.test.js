86590d2779a4ef6b92f77671a5e9be737c3d4d84
Marija K <kccmrj@outlook.com>
2023-03-16 22:24:12 +0100

    added tests for useConfirmAccount hook

---
+++ frontend/src/hooks/__tests__/useConfirmAccount.test.js
@@ -0,0 +1,39 @@
+import { renderHook, act, cleanup } from "@testing-library/react";
+import { server, rest } from "../../mocks/server";
+import { useConfirmAccount } from "../useConfirmAccount"
+
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => server.close());
+
+describe("useConfirmAccount()", () => {
+ it("should have success and error states initially set to null", () => {
+    const { result } = renderHook(useConfirmAccount);
+    expect(result.current.error).toBeFalsy();
+    expect(result.current.success).toBeFalsy();
+ });
+
+ it("should set error state to true given that the server responded with an error", async () => {
+    server.use(
+      rest.get("api/users/:accountConfirmationToken", (req, res, ctx) => {
+        return res(
+          ctx.status(404),
+          ctx.json({
+            error: "Expired token or already confirmed",
+          })
+        );
+      }));
+    const { result } = renderHook(useConfirmAccount);
+    await act(async () => result.current.confirmAccount());
+    await expect(result.current.error).toBeTruthy(); 
+   });
+
+    it("should set success state to true given that the server responded with a success response", async () => {
+      const { result } = renderHook(useConfirmAccount);
+      await act(async () => result.current.confirmAccount());
+      await expect(result.current.success).toBeTruthy();
+    });
+})
