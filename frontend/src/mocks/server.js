d027d4e0e0761638b95debc1025db1b15ab8ad85
Marija K <kccmrj@outlook.com>
2023-03-16 22:23:00 +0100

    added confirm account handler

--- frontend/src/mocks/server.js
+++ frontend/src/mocks/server.js
@@ -4,14 +4,21 @@ import { rest } from "msw";
 
 const handlers = [
   rest.post("api/users/signup", (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          success: "Account created and pending confirmation",
-        })
-      );  
-    }
-  ),
+    return res(
+      ctx.status(200),
+      ctx.json({
+        success: "Account created and pending confirmation",
+      })
+    );
+  }),
+  rest.get("api/users/:accountConfirmationToken", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        success: "Account confirmed, you may log in",
+      })
+    );
+  }),
 ];
 
 // This configures a request mocking server with the given request handlers.
