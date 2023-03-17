936db7f57625871066e561221f14308dd6b84542
Marija K <kccmrj@outlook.com>
2023-03-17 16:51:41 +0100

    Update ConfirmedAccount.js

--- frontend/src/pages/ConfirmedAccount.js
+++ frontend/src/pages/ConfirmedAccount.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import useConfirmAccount from '../hooks/useConfirmAccount';
+import { useConfirmAccount } from '../hooks/useConfirmAccount';
 
 export default function ConfirmedAccount() {
   const [token, setToken] = React.useState(null);
@@ -22,14 +22,14 @@ export default function ConfirmedAccount() {
   return (
     <div className="confirmed--container">
       {success && (
-        <div className="confirmed--account--success">
+        <div role="alert" className="confirmed--account--success">
           <h2>Your account has been confirmed.</h2>
           <p>You may log in now.</p>
         </div>
       )}
       {error && (
         <>
-          <div className="error">{error} </div>
+          <div role="alert" className="error">{error} </div>
           <div className="already--confirmed">
             <p>In fact, you might be seeing this because the page was refreshed
             after your account has already been confirmed!</p>
