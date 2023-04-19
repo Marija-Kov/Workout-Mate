c9789b060d80194d5e6446b7c2bfefa88e31949c
Marija K <kccmrj@outlook.com>
2023-04-19 20:16:21 +0200

    make sure the entire localStorage is cleared

--- frontend/src/utils/logOutIfTokenExpired.js
+++ frontend/src/utils/logOutIfTokenExpired.js
@@ -4,6 +4,12 @@ export const logOutIfTokenExpired = () => {
      const tokenExpires = JSON.parse(localStorage.getItem("user")).tokenExpires;
    if (tokenExpires < Date.now()) {
      localStorage.removeItem("user");
+     if(localStorage.getItem("username")){
+      localStorage.removeItem("username");
+     }
+     if(localStorage.getItem("newImg")) {
+       localStorage.removeItem("newImg");
+     }
      window.location.reload();
     } 
   }
