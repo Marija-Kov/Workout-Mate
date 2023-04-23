a7d1235ef4d7be45a6efa5e661735fd692299ac1
Marija K <kccmrj@outlook.com>
2023-04-23 14:34:21 +0200

    Update server.js

--- frontend/src/mocks/server.js
+++ frontend/src/mocks/server.js
@@ -1,9 +1,9 @@
-// src/mocks/server.js
 import { setupServer } from "msw/node";
 import { rest } from "msw";
+import uuid from "react-uuid";
 
 const handlers = [
-  rest.post("api/users/signup", (req, res, ctx) => {
+  rest.post("/api/users/signup", (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
@@ -12,7 +12,7 @@ const handlers = [
     );
   }),
 
-  rest.get("api/users/*", (req, res, ctx) => {
+  rest.get("/api/users/*", (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
@@ -21,19 +21,57 @@ const handlers = [
     );
   }),
 
-  rest.post("api/users/login", (req, res, ctx) => {
+  rest.post("/api/users/login", async (req, res, ctx) => {
+    const { email } = await req.json();
     return res(
       ctx.status(200),
       ctx.json({
         id: "userid",
-        email: "email",
-        token: "authorizationToken"
+        email: email,
+        token: "authorizationToken",
+        tokenExpires: Date.now() + 3600000,
       })
+    );
+  }),
+
+  rest.all("/api/workouts/", (req, res, ctx) => {
+    console.log(req)
+    // const page = req.query.p || 0;
+    // const search = req.query.search || null;
+    // const sampleWorkouts = genSampleWorkouts(search, page);
+    // const itemsPerPage = 3;
+    return res(
+      ctx.status(200),
+      // ctx.json({
+      //   allUserWorkoutsByQuery: sampleWorkouts,
+      //   workoutsChunk: sampleWorkouts.slice(itemsPerPage),
+      //   limit: itemsPerPage,
+      //   noWorkoutsByQuery: false,
+      // })
     );
   }),
 ];
 
-// This configures a request mocking server with the given request handlers.
+function genSampleWorkouts(searchFor, page){
+  const workoutTitles = ['bench press', 'pullups', 'pushups', 'burpees', 'squats', 'arm curls'];
+  const workouts = [];
+  for(let i = 0; i < workoutTitles.length; ++i){
+    workouts.push({
+      _id: uuid(),
+      title: workoutTitles[i],
+      reps: Math.floor(Math.random() * 99) + 1,
+      load: Math.floor(Math.random() * 50),
+      user_id: "userid"
+    });
+  }
+  return workouts.filter(e => {
+    const regExp = `^${searchFor}`;
+    e.title.match(regExp);
+    //take page number into account
+  })
+}
+
 const server = setupServer(...handlers);
 
-export { server, rest }
+export { server, rest, genSampleWorkouts }
+
