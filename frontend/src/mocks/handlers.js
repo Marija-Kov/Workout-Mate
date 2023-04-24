f6ef05680c85da426fa5c4d1ccd31c500b31e82d
Marija K <kccmrj@outlook.com>
2023-04-24 22:25:10 +0200

    Create handlers.js

---
+++ frontend/src/mocks/handlers.js
@@ -0,0 +1,52 @@
+import { rest } from "msw";
+import { genSampleWorkouts } from "../utils/test/genSampleWorkouts";
+
+export const handlers = [
+  rest.post("/api/users/signup", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        success: "Account created and pending confirmation",
+      })
+    );
+  }),
+
+  rest.get("/api/users/*", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        success: "Account confirmed, you may log in",
+      })
+    );
+  }),
+
+  rest.post("/api/users/login", async (req, res, ctx) => {
+    const { email } = await req.json();
+    return res(
+      ctx.status(200),
+      ctx.json({
+        id: "userid",
+        email: email,
+        token: "authorizationToken",
+        tokenExpires: Date.now() + 3600000,
+      })
+    );
+  }),
+
+  rest.all("/api/workouts/", (req, res, ctx) => {
+    const page = req.query.p || 0;
+    const search = req.query.search || null;
+    const itemsPerPage = 3;
+    const { allUserWorkoutsByQuery, workoutsChunk, noWorkoutsByQuery } =
+      genSampleWorkouts(search, page, itemsPerPage);
+    return res(
+      ctx.status(200),
+      ctx.json({
+        allUserWorkoutsByQuery: allUserWorkoutsByQuery,
+        workoutsChunk: workoutsChunk,
+        limit: itemsPerPage,
+        noWorkoutsByQuery: noWorkoutsByQuery,
+      })
+    );
+  }),
+];
