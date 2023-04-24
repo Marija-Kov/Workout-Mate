902a90ece5c6720fb9078da37aa139a7981c6fe0
Marija K <kccmrj@outlook.com>
2023-04-24 22:25:34 +0200

    separate handlers from mock server

--- frontend/src/mocks/server.js
+++ frontend/src/mocks/server.js
@@ -1,57 +1,6 @@
 import { setupServer } from "msw/node";
-import { rest } from "msw";
-import { genSampleWorkouts } from "../utils/test/genSampleWorkouts";
+import { handlers } from "./handlers";
 
-const handlers = [
-  rest.post("/api/users/signup", (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        success: "Account created and pending confirmation",
-      })
-    );
-  }),
+export const server = setupServer(...handlers);
 
-  rest.get("/api/users/*", (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        success: "Account confirmed, you may log in",
-      })
-    );
-  }),
-
-  rest.post("/api/users/login", async (req, res, ctx) => {
-    const { email } = await req.json();
-    return res(
-      ctx.status(200),
-      ctx.json({
-        id: "userid",
-        email: email,
-        token: "authorizationToken",
-        tokenExpires: Date.now() + 3600000,
-      })
-    );
-  }),
-
-  rest.all("/api/workouts/", (req, res, ctx) => {
-    const page = req.query.p || 0;
-    const search = req.query.search || null;
-    const itemsPerPage = 3;
-    const { allUserWorkoutsByQuery, workoutsChunk, noWorkoutsByQuery}= genSampleWorkouts(search, page, itemsPerPage);
-    return res(
-      ctx.status(200),
-      ctx.json({
-        allUserWorkoutsByQuery: allUserWorkoutsByQuery,
-        workoutsChunk: workoutsChunk,
-        limit: itemsPerPage,
-        noWorkoutsByQuery: noWorkoutsByQuery,
-      })
-    );
-  }),
-];
-
-const server = setupServer(...handlers);
-
-export { server, rest }
 
