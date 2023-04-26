0f0d6a1dbb45eff92199ac27084e0232a17060a8
Marija K <kccmrj@outlook.com>
2023-04-26 11:47:26 +0200

    Create ForgotPasswordForm.test.js

---
+++ frontend/src/components/__tests__/ForgotPasswordForm.test.js
@@ -0,0 +1,80 @@
+import ForgotPasswordForm from "../ForgotPasswordForm";
+import user from "@testing-library/user-event";
+import { render, screen, cleanup } from "@testing-library/react";
+import { rest } from "msw";
+import { server } from "../../mocks/server";
+
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => server.close());
+
+describe("<ForgotPasswordForm />", () => {
+  it("should render ForgotPasswordForm component properly", async () => {
+    render(<ForgotPasswordForm />);
+    const inputField = await screen.findByLabelText(/email address/i);
+    const submitBtn = await screen.findByLabelText(/submit/i);
+    const closeBtn = await screen.findByLabelText(/close/i);
+    expect(inputField).toBeInTheDocument();
+    expect(submitBtn).toBeInTheDocument();
+    expect(closeBtn).toBeInTheDocument();
+  });
+
+  it("should focus form elements in the right order", async () => {
+    user.setup();
+    render(<ForgotPasswordForm />);
+    const inputField = await screen.findByLabelText(/email address/i);
+    const submitBtn = await screen.findByLabelText(/submit/i);
+    const closeBtn = await screen.findByLabelText(/close/i);
+    await user.tab();
+    expect(inputField).toHaveFocus();
+    await user.tab();
+    expect(submitBtn).toHaveFocus();
+    await user.tab();
+    //TODO: make closeBtn focusable
+  });
+
+  it("should update input value when user types", async () => {
+    user.setup();
+    render(<ForgotPasswordForm />);
+    const inputField = await screen.findByLabelText(/email address/i);
+    await user.type(inputField, "keech");
+    expect(inputField).toHaveValue("keech");
+  });
+
+  it("should render error message if user attempts to submit invalid input", async () => {
+    server.use(
+      rest.post("/api/reset-password", (req, res, ctx) => {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: "Invalid input or user not confirmed",
+          })
+        );
+      })
+    );
+    user.setup();
+    render(<ForgotPasswordForm />);
+    const inputField = await screen.findByLabelText(/email address/i);
+    const submitBtn = await screen.findByLabelText(/submit/i);
+    await user.type(inputField, "keech");
+    await user.click(submitBtn);
+    const error = await screen.findByRole("alert");
+    expect(error).toBeInTheDocument();
+    expect(error).toHaveAttribute("class", "error");
+  });
+
+  it("should render success message if user submits valid email", async () => {
+    user.setup();
+    render(<ForgotPasswordForm />);
+    const inputField = await screen.findByLabelText(/email address/i);
+    const submitBtn = await screen.findByLabelText(/submit/i);
+    await user.type(inputField, "keech@mail.yu");
+    await user.click(submitBtn);
+    const error = await screen.findByRole("alert");
+    expect(error).toBeInTheDocument();
+    expect(error).toHaveAttribute("class", "success");
+  });
+})
