78f1dc6c54692d613d06c7cf5f896cbdf5087bb0
Marija K <kccmrj@outlook.com>
2023-04-29 13:30:00 +0200

    Update Search.js

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -4,12 +4,13 @@ export default function Search({handleSearch, query, isLoading, handleSearchChan
   return (
     <form aria-label="search bar" className="search--bar" onSubmit={handleSearch}>
       <input
+        aria-label="search input"
         type="search"
         placeholder="search workouts..."
         value={query}
         onChange={handleSearchChange}
       ></input>
-      <button disabled={isLoading}>
+      <button aria-label="search button" disabled={isLoading}>
         <span className="material-symbols-outlined">search</span>
       </button>
     </form>
