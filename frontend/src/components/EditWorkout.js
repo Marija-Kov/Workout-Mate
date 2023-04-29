19b6b5c3768db85ea96f6a7c04c8e6c0e02f1c3d
Marija K <kccmrj@outlook.com>
2023-04-29 11:32:49 +0200

    Update EditWorkout.js

--- frontend/src/components/EditWorkout.js
+++ frontend/src/components/EditWorkout.js
@@ -33,52 +33,65 @@ export default function EditWorkout(props){
  }
     return (
       <div className="form--container">
-      <form className="edit--form" aria-label="edit workout form">
-        <button
-          aria-label="close form"
-          className="close material-symbols-outlined"
-          onClick={() => props.showEdit()}
+        <form
+          className="edit--form"
+          aria-label={`edit ${props.title} ${
+            props.updatedAt
+              ? `updated ${props.updatedAt}`
+              : `created ${props.createdAt}`
+          }`}
         >
-          close
-        </button>
-        <h4>Edit workout</h4>
-        <label>exercise title:</label>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          aria-label="workout title"
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-          className={emptyFields.includes("title") ? "error" : ""}
-        />
-        <label>number of reps:</label>
-        <input
-          type="number"
-          name="reps"
-          id="reps"
-          aria-label="number of reps"
-          onChange={(e) => setReps(e.target.value)}
-          value={reps}
-          className={emptyFields.includes("reps") ? "error" : ""}
-        />
-        <label>load (kg):</label>
-        <input
-          type="number"
-          name="load"
-          id="load"
-          aria-label="load in kg"
-          onChange={(e) => setLoad(e.target.value)}
-          value={load}
-          className={emptyFields.includes("load") ? "error" : ""}
-        />
-          <button className="edit--form--btn" onClick={(e) => handleUpdate(e)} aria-label="submit edited workout button" >Save changes</button>
-        {error && (
-          <div role="alert" className="error">
-            {error}
-          </div>
-        )}
-      </form>
-     </div>
+          <button
+            aria-label="close form"
+            className="close material-symbols-outlined"
+            onClick={() => props.showEdit()}
+          >
+            close
+          </button>
+          <h4>Edit workout</h4>
+          <label>exercise title:</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            aria-label="workout title"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            className={emptyFields.includes("title") ? "error" : ""}
+          />
+          <label>number of reps:</label>
+          <input
+            type="number"
+            name="reps"
+            id="reps"
+            aria-label="number of reps"
+            onChange={(e) => setReps(e.target.value)}
+            value={reps}
+            className={emptyFields.includes("reps") ? "error" : ""}
+          />
+          <label>load (kg):</label>
+          <input
+            type="number"
+            name="load"
+            id="load"
+            aria-label="load in kg"
+            onChange={(e) => setLoad(e.target.value)}
+            value={load}
+            className={emptyFields.includes("load") ? "error" : ""}
+          />
+          <button
+            className="edit--form--btn"
+            onClick={(e) => handleUpdate(e)}
+            aria-label="submit edited workout button"
+          >
+            Save changes
+          </button>
+          {error && (
+            <div role="alert" className="error">
+              {error}
+            </div>
+          )}
+        </form>
+      </div>
     );
 }
