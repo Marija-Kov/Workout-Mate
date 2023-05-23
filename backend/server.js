34f39141b8735ece74af12bb6c0c76b46fc6e833
Marija K <kccmrj@outlook.com>
2023-05-23 16:32:31 +0200

    copy frontend build to backend and make express use static file
    this is supposed to enable serving the entire app from one origin

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const cors = require("cors");
+
 
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
@@ -10,19 +10,16 @@ const userRoutes = require('./routes/users');
 const passwordResetRoutes = require('./routes/resetPassword')
 
 const app = express();
-
+app.use(express.static('build'));
 app.use(express.json({ limit: "50mb" })); 
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
 );
 
-app.use(cors({
-  origin: process.env.CLIENT_URL
-}))
-// app.use((req, res, next)=>{
-//     console.log(req.path, req.method);
-//     next();
-// })
+app.use((req, res, next)=>{
+    console.log(req.path, req.method);
+    next();
+})
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
