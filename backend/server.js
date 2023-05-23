6ccda4e621edf853576b2600015496425cd616ae
Marija K <kccmrj@outlook.com>
2023-05-23 22:19:01 +0200

    remove build from backend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const userRoutes = require('./routes/users');
 const passwordResetRoutes = require('./routes/resetPassword')
 
 const app = express();
-app.use(express.static('build'));
+
 app.use(express.json({ limit: "50mb" })); 
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
