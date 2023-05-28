ff8d061a63bd1c91496bfcae9fad31ee5032469b
Marija K <kccmrj@outlook.com>
2023-05-28 15:31:07 +0200

    add error element, make it fit nicely on the page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutForm from '../components/WorkoutForm'
 import { useWorkoutContext } from '../hooks/useWorkoutContext'
-import { useAuthContext } from "../hooks/useAuthContext";
 import Pagination from '../components/Pagination';
 import { useSearch } from '../hooks/useSearch';
 import Search from '../components/Search';
@@ -11,8 +10,7 @@ import { logOutIfTokenExpired } from '../utils/logOutIfTokenExpired';
 export default function Home() {
     const [addWorkoutForm, setAddWorkoutForm] = React.useState(false);
     const { workouts } = useWorkoutContext(); 
-    const { user } = useAuthContext();
-    const { search, total, limit, isLoading } = useSearch();
+    const { search, total, limit, isLoading, error } = useSearch();
     const [page, setPage] = React.useState(0);
     const [pageSpread, setPageSpread] = React.useState([]);
     const [query, setQuery] = React.useState("");
@@ -62,72 +60,76 @@ export default function Home() {
 
     return (
       <div className="home--container" onClick={logOutIfTokenExpired}>
-          <div className="home">
-            <Search
-              handleSearchChange={handleSearchChange}
-              handleSearch={handleSearch}
-              query={query}
-              isLoading={isLoading}
-            />
-
-            <div aria-label="workouts" className="workouts--container">
-              {workouts &&
-                workouts.map((workout) => (
-                  <WorkoutDetails
-                    key={workout._id}
-                    id={workout._id}
-                    title={workout.title}
-                    reps={workout.reps}
-                    load={workout.load}
-                    createdAt={workout.createdAt}
-                    updatedAt={workout.updatedAt}
-                    page={page}
-                    getItems={getItems}
-                    spreadPages={spreadPages}
-                    total={total}
-                    limit={limit}
-                  />
-                ))}
-            </div>
-            {!addWorkoutForm && (
-              <button
-                aria-label="buff it up"
-                className="add--workout"
-                onClick={() => setAddWorkoutForm(true)}
-              >
-                + Buff It Up
-              </button>
+        <div className="home">
+          <Search
+            handleSearchChange={handleSearchChange}
+            handleSearch={handleSearch}
+            query={query}
+            isLoading={isLoading}
+          />
+            {error && (
+              <div role="alert" className="error">
+                {error}
+              </div>
             )}
-            <Pagination
-              page={page}
-              pageSpread={pageSpread}
+          <div aria-label="workouts" className="workouts--container">
+            {workouts &&
+              workouts.map((workout) => (
+                <WorkoutDetails
+                  key={workout._id}
+                  id={workout._id}
+                  title={workout.title}
+                  reps={workout.reps}
+                  load={workout.load}
+                  createdAt={workout.createdAt}
+                  updatedAt={workout.updatedAt}
+                  page={page}
+                  getItems={getItems}
+                  spreadPages={spreadPages}
+                  total={total}
+                  limit={limit}
+                />
+              ))}
+          </div>
+          {!addWorkoutForm && (
+            <button
+              aria-label="buff it up"
+              className="add--workout"
+              onClick={() => setAddWorkoutForm(true)}
+            >
+              + Buff It Up
+            </button>
+          )}
+          <Pagination
+            page={page}
+            pageSpread={pageSpread}
+            total={total}
+            limit={limit}
+            flipPage={flipPage}
+          />
+
+          {addWorkoutForm && (
+            <WorkoutForm
+              hideForm={hideForm}
+              getItems={getItems}
+              spreadPages={spreadPages}
+              flipPage={flipPage}
               total={total}
               limit={limit}
-              flipPage={flipPage}
             />
-
-            {addWorkoutForm && (
-              <WorkoutForm
-                hideForm={hideForm}
-                getItems={getItems}
-                spreadPages={spreadPages}
-                flipPage={flipPage}
-                total={total}
-                limit={limit}
-              />
-            )}
-            <div className="space"></div>
-            <div className="chart--container">
-              <h3>Routine Balance</h3>
-              <div className="chart"></div>
-              <p className="stats--upper-bod">
-                <span></span> Upper body - 36%
-              </p>
-              <p className="stats--lower-bod">
-                <span></span> Lower body - 64%
-              </p>
-            </div>
+          )}
+          <div className="space"></div>
+          <div className="chart--container">
+            <h3>Routine Balance</h3>
+            <div className="chart"></div>
+            <p className="stats--upper-bod">
+              <span></span> Upper body - 36%
+            </p>
+            <p className="stats--lower-bod">
+              <span></span> Lower body - 64%
+            </p>
           </div>
+        </div>
       </div>
     );
 };
