3c3d222c5aa91a9c4ac4927cb92c2f560f25ae84
Marija K <kccmrj@outlook.com>
2023-05-28 15:14:26 +0200

    refactor to accept env variables

--- backend/middleware/rateLimiters.js
+++ backend/middleware/rateLimiters.js
@@ -1,8 +1,14 @@
 const rateLimit = require("express-rate-limit");
 
 module.exports.api_users = rateLimit({
-  windowMs: 600000,
-  max: 10,
+  windowMs:
+    process.env.NODE_ENV !== "test"
+      ? Number(process.env.API_USERS_WINDOW_MS)
+      : Number(process.env.TEST_API_USERS_WINDOW_MS),
+  max:
+    process.env.NODE_ENV !== "test"
+      ? Number(process.env.MAX_API_USERS_REQS)
+      : Number(process.env.TEST_MAX_API_USERS_REQS),
   message: async (req, res) => {
     res.status(429).json({ error: "Too many requests. Please try later." });
   },
@@ -11,8 +17,14 @@ module.exports.api_users = rateLimit({
 });
 
 module.exports.api_reset_password = rateLimit({
-  windowMs: 600000,
-  max: 5,
+  windowMs:
+    process.env.NODE_ENV !== "test"
+      ? Number(process.env.API_RESET_PASSWORD_WINDOW_MS)
+      : Number(process.env.TEST_API_RESET_PASSWORD_WINDOW_MS),
+  max:
+    process.env.NODE_ENV !== "test"
+      ? Number(process.env.MAX_API_RESET_PASSWORD_REQS)
+      : Number(process.env.TEST_MAX_API_RESET_PASSWORD_REQS),
   message: async (req, res) => {
     res.status(429).json({ error: "Too many requests. Please try later." });
   },
@@ -21,8 +33,14 @@ module.exports.api_reset_password = rateLimit({
 });
 
 module.exports.api_workouts = rateLimit({
-  windowMs: 600000,
-  max: 100,
+  windowMs:
+    process.env.NODE_ENV !== "test"
+      ? Number(process.env.API_WORKOUTS_WINDOW_MS)
+      : Number(process.env.TEST_API_WORKOUTS_WINDOW_MS),
+  max:
+    process.env.NODE_ENV !== "test"
+      ? Number(process.env.MAX_API_WORKOUTS_REQS)
+      : Number(process.env.TEST_MAX_API_WORKOUTS_REQS),
   message: async (req, res) => {
     res.status(429).json({ error: "Too many requests. Please try later." });
   },
