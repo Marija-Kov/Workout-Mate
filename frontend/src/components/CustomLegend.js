8ecab220d39d3387b9dc321b419104ee40787ae3
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-07-02 19:34:47 +0200

    Change label style depending on percentage..
    Use lighter text color if percentage for a muscle group is 0.

--- frontend/src/components/CustomLegend.js
+++ frontend/src/components/CustomLegend.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
- const CustomLegend = ({labels, colors}) => {
+ const CustomLegend = ({labels, colors, percentage}) => {
   return (
     <ul className="custom--legend">
         {labels.map((label, index) => (
-           <li key={label+index} className="muscle--group">
+           <li key={label+index} className={percentage[index] > 0 ? "muscle--group" : "muscle--group zero--percent"}>
              <span style={{backgroundColor: colors[index]}}></span>
                {label}
            </li> 
