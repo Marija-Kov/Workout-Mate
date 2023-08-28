79b28c494fb98c5f504df29054db0c08a546424b
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-08-28 21:38:58 +0200

    Refactored

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { useSearch } from "../hooks/useSearch";
 
-export default function Search({handleSearch, query, isLoading, handleSearchChange}) {
+export default function Search() {
+  const { loading } = useSelector(state => state.workout);
+  const dispatch = useDispatch();
+  const page = useSelector(state => state.page);
+  const query = useSelector(state => state.query);
+  const { search } = useSearch();
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    await search(query, page);
+  };
+
   return (
-    <form aria-label="search bar" className={isLoading ? "search--bar is--loading" : "search--bar"} onSubmit={handleSearch}>
+    <form aria-label="search bar" className={loading ? "search--bar is--loading" : "search--bar"} onSubmit={handleSearch}>
       <input
         aria-label="search input"
         type="search"
         placeholder="search workouts..."
         value={query}
-        onChange={handleSearchChange}
-        disabled={isLoading}
+        onChange={(e) => {
+          dispatch({type: "SET_QUERY", payload: e.target.value});
+       }}
+        disabled={loading}
       ></input>
-      <button aria-label="search button" disabled={isLoading}>
+      <button aria-label="search button" disabled={loading}>
         <span className="material-symbols-outlined">search</span>
       </button>
     </form>
