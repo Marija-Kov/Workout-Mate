101a850a1f5e93acd02f78de5fd7aa0efc25f18c
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-08-29 22:59:41 +0200

    Update tests

--- frontend/src/hooks/__tests__/useEditWorkout.test.js
+++ frontend/src/hooks/__tests__/useEditWorkout.test.js
@@ -1,138 +1,115 @@
 import { renderHook, act } from "@testing-library/react";
 import { rest } from "msw";
 import { server } from "../../mocks/server";
-import { AuthContext } from "../../context/AuthContext";
-import { WorkoutContext } from "../../context/WorkoutContext";
 import useEditWorkout from "../useEditWorkout";
+import { Provider } from "react-redux";
+import store from '../../redux/store';
+
+let wrapper;
+let dispatch;
+let mockUser;
+
+beforeAll(() => {
+  wrapper = ({ children }) => {
+    return (
+      <Provider store={store}>
+          {children}
+      </Provider>
+    );
+  };
+  dispatch = store.dispatch;
+  mockUser = {
+    id: "userid",
+    email: "keech@mail.yu",
+    token: "authorizationToken",
+    username: undefined,
+    profileImg: undefined,
+    tokenExpires: Date.now() + 3600000,
+  };
+})
+
+afterAll(() => {
+  wrapper = null;
+  dispatch = null;
+  mockUser = null;
+})
 
 describe("useEditWorkout()", () => {
-  it("should return editWorkout function and error state set to 'false'", () => {
-    const wrapper = ({ children }) => {
-      return (
-        <AuthContext.Provider value={{ user: {} }}>
-          <WorkoutContext.Provider value={{ workouts: [] }}>
-            {children}
-          </WorkoutContext.Provider>
-        </AuthContext.Provider>
-      );
-    };
+  it("should return editWorkout function", () => {
     const { result } = renderHook(useEditWorkout, { wrapper });
-    expect(result.current.error).toBeFalsy();
     expect(result.current.editWorkout).toBeTruthy();
+    expect(typeof result.current.editWorkout).toBe("function");
   });
 
-  it("should not change default error state (false) when editWorkout was run with authorization and valid input", async () => {
-    const mockUpdateWorkout = { title: "squats", load: 15 };
-    const mockOldWorkout = {
+  it("should update workout given that editWorkout was run with authorization and valid input", async () => {
+    const mockWorkout = {
       id: "mockId",
       title: "lunges",
       muscle_group: "leg",
       reps: "44",
       load: "21",
-      user_id: {}.id
-    }
-    const closeEdit = () => {};
-    const wrapper = ({ children }) => {
-      return (
-        <AuthContext.Provider value={{ user: {} }}>
-          <WorkoutContext.Provider
-            value={{ workouts: [], dispatch: () => {} }}
-          >
-            {children}
-          </WorkoutContext.Provider>
-        </AuthContext.Provider>
-      );
-    };
-    const { result } = renderHook(useEditWorkout, { wrapper });
-    await act(() =>
-      result.current.editWorkout(
-        mockOldWorkout.id,
-        mockUpdateWorkout,
-        closeEdit
-      )
-    );
-    expect(result.current.error).toBeFalsy();
-  });
-
-  it("should set error state to 'true' given that request wasn't authorized", async () => {
-    const mockUpdateWorkout = { title: "squats", load: 15 };
-    const mockOldWorkout = {
-      id: "mockId",
-      title: "lunges",
-      muscle_group: "leg",
-      reps: "44",
-      load: "21",
-      user_id: {}.id
-    }
-    const closeEdit = () => {};
-    const wrapper = ({ children }) => {
-      return (
-        <AuthContext.Provider value={{ user: null }}>
-          <WorkoutContext.Provider
-            value={{ workouts: [], dispatch: () => {} }}
-          >
-            {children}
-          </WorkoutContext.Provider>
-        </AuthContext.Provider>
-      );
-    };
-    const { result } = renderHook(useEditWorkout, { wrapper });
-    await act(() =>
-      result.current.editWorkout(
-        mockOldWorkout.id,
-        mockUpdateWorkout,
-        closeEdit
-      )
-    );
-    expect(result.current.error).toBeTruthy();
-    expect(result.current.error).toMatch(/must be logged in/i);
-  });
-
-    it("should set error state to 'true' given that input was invalid", async () => {
-      const mockUpdateWorkout = { title: "", load: 15 };
-      const mockOldWorkout = {
-        id: "mockId",
-        title: "lunges",
-        muscle_group: "leg",
-        reps: "44",
-        load: "21",
-        user_id: {}.id
-      }
-      const closeEdit = () => {};
-      server.use(
-        rest.patch(
-          `${process.env.REACT_APP_API}/api/workouts/*`,
-          (req, res, ctx) => {
-            return res(
-              ctx.status(400),
-              ctx.json({
-                error: "all fields must be filled",
-              })
-            );
-          }
-        )
-      );
-      const wrapper = ({ children }) => {
-        return (
-          <AuthContext.Provider value={{ user: null }}>
-            <WorkoutContext.Provider
-              value={{ workouts: [], dispatch: () => {} }}
-            >
-              {children}
-            </WorkoutContext.Provider>
-          </AuthContext.Provider>
+      user_id: "userid"
+     }
+    const mockUpdate = { title: "squats" };
+    server.use(
+      rest.patch(`${process.env.REACT_APP_API}/api/workouts/*`, (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            title: mockUpdate.title,
+          })
         );
-      };
-      const { result } = renderHook(useEditWorkout, { wrapper });
-      await act(() =>
-        result.current.editWorkout(
-          mockOldWorkout.id,
-          mockUpdateWorkout,
+      }),
+    )
+    dispatch({type: "LOGIN_SUCCESS", payload: mockUser})
+    dispatch({type: "CREATE_WORKOUT_SUCCESS", payload: mockWorkout})
+    let state = store.getState()
+    expect(state.workout.workouts.workoutsChunk[0].title).toMatch(/lunges/i);
+    const closeEdit = () => {};
+    const { result } = renderHook(useEditWorkout, { wrapper });
+    await act(() =>
+    result.current.editWorkout(
+          mockWorkout.id,
+          mockUpdate,
           closeEdit
         )
-      );
-      expect(result.current.error).toBeTruthy();
-      expect(result.current.error).toMatch(/must be logged in/i);
-    });
+        );
+        state = store.getState()
+        expect(state.workout.updateWorkoutError).toBeFalsy();
+        expect(state.workout.workouts.workoutsChunk[0].title).toMatch(mockUpdate.title);
+        dispatch({type: "DELETE_ALL_WORKOUTS_SUCCESS", payload: "success"});
+        dispatch({type: "LOGOUT"})
+      });
+  
+  it("should set updateWorkoutError message given that request wasn't authorized", async () => {
+    const mockWorkout = {
+      id: "mockId",
+      title: "lunges",
+      muscle_group: "leg",
+      reps: "44",
+      load: "21",
+      user_id: "userid"
+     }
+    const mockUpdate = { title: "squats" };
+    dispatch({type: "LOGIN_SUCCESS", payload: mockUser});
+    dispatch({type: "CREATE_WORKOUT_SUCCESS", payload: mockWorkout});
+    let state = store.getState()
+    expect(state.workout.workouts.workoutsChunk[0].title).toMatch(mockWorkout.title);
+    dispatch({type: "LOGOUT"});
+    const closeEdit = () => {};
+    const { result } = renderHook(useEditWorkout, { wrapper });
+    await act(() =>
+    result.current.editWorkout(
+      mockWorkout.id,
+      mockUpdate,
+      closeEdit
+    )
+    );
+    state = store.getState()
+    expect(state.workout.workouts.workoutsChunk[0].title).toMatch(mockWorkout.title);
+    expect(state.workout.updateWorkoutError).toBeTruthy();
+    expect(state.workout.updateWorkoutError).toMatch(/you must be logged in/i);
+    dispatch({type: "DELETE_ALL_WORKOUTS_SUCCESS", payload: "All workouts deleted successfully"});
+  });
 
 });
