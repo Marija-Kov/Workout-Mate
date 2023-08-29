6b0186c02f7cdc989b699de9ce13c78735155178
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-08-29 10:29:24 +0200

    Refactor

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,73 +12,32 @@ import { useSelector } from 'react-redux';
 
 export default function Home() {
     const [addWorkoutForm, setAddWorkoutForm] = React.useState(false);
-    const { workouts } = useWorkoutContext(); 
-    const { search, total, limit, allWorkoutsMuscleGroups, isLoading, error } = useSearch();
-    const [page, setPage] = React.useState(0);
-    const [pageSpread, setPageSpread] = React.useState([]);
     const { workouts, loading, setWorkoutsError } = useSelector(state => state.workout);
+    const page = useSelector(state => state.page);
+    const query = useSelector(state => state.query);
     const { total, allUserWorkoutsMuscleGroups, workoutsChunk } = workouts;
     const { search } = useSearch();
-    const [query, setQuery] = React.useState("");
 
     React.useEffect(() => {
-      getItems(query, page);
+      search(query, page);
     }, [query, page]);
-
-    React.useEffect(()=> {
-      spreadPages(total, limit)
-    },[total,limit])
-
-    const getItems = async (q,p) => {
-      await search(q, p);
-    };
-    
-    const spreadPages = (t, l) => {
-      const pagesNum = Math.ceil(t / l);
-      let spread = [];
-      for (let i = 1; i <= pagesNum; ++i) {
-        spread.push(i);
-      }
-      setPageSpread(spread);
-    };
 
    const hideForm = () => {
        setAddWorkoutForm(false)
    }
 
-   const flipPage = (num) => {
-     setPage((prev) => {
-       if (num === -1) return prev + num;
-       if (num[0]) return prev + num[0];
-       return num - 1;
-     });
-   };
-
-   const handleSearch = async (e) => {
-     e.preventDefault();
-     await getItems(query, page);
-   };
-
-   const handleSearchChange = (e) => {
-      setQuery(e.target.value);
-      setPage(0);
-   }
-
-   const muscleGroups = React.useMemo(() => allWorkoutsMuscleGroups, [allWorkoutsMuscleGroups.length])
+   const muscleGroups = React.useMemo(() => allUserWorkoutsMuscleGroups, [allUserWorkoutsMuscleGroups])
 
     return (
       <div className="home--container" onClick={logOutIfTokenExpired}>
         <div className="home">
-          <Search
-            handleSearchChange={handleSearchChange}
-            handleSearch={handleSearch}
-            query={query}
-          />
+          <Search/>
             {setWorkoutsError && (
               <div role="alert" className="error">
                 {setWorkoutsError}
               </div>
             )}
+
           <div aria-label="workouts" className="workouts--container">
             {workoutsChunk ?
               workoutsChunk.map((workout) => (
@@ -91,20 +50,18 @@ export default function Home() {
                   load={workout.load}
                   createdAt={workout.createdAt}
                   updatedAt={workout.updatedAt}
-                  page={page}
-                  getItems={getItems}
-                  spreadPages={spreadPages}
-                  total={total}
-                  limit={limit}
                 />
               )) : <WorkoutsPlaceholder />}
+
              {workoutsChunk && !workoutsChunk.length && !loading && 
               <h4 className="get--started">
                 {!workoutsChunk && !query && <>Buff it up to get started.<br></br>No pressure <span>🥤</span></>} 
                 {query && <>No "{query}" workouts found.</>} 
               </h4>}
            </div>
+
           {workoutsChunk ? <Chart muscleGroups={muscleGroups}/> : <ChartPlaceholder />}
+          
           {!addWorkoutForm && (
             <button
               aria-label="buff it up"
@@ -123,25 +80,10 @@ export default function Home() {
             </button>
           )}
 
-          {workouts && workouts.length > 0 &&
-          <Pagination
-            page={page}
-            pageSpread={pageSpread}
-            total={total}
-            limit={limit}
-            flipPage={flipPage}
-          />}
+          {total && <Pagination />}
           
-          {addWorkoutForm && (
-            <WorkoutForm
-              hideForm={hideForm}
-              getItems={getItems}
-              spreadPages={spreadPages}
-              flipPage={flipPage}
-              total={total}
-              limit={limit}
-            />
-          )}
+          {addWorkoutForm && <WorkoutForm hideForm={hideForm}/>}
+
           <div className="space"></div>
           
         </div>
