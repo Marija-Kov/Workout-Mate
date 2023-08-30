2fc3f22abc4ba6cdf3f7e9d3c92560684c34e374
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-08-30 20:40:12 +0200

    Update tests

--- frontend/src/hooks/__tests__/useSearch.test.js
+++ frontend/src/hooks/__tests__/useSearch.test.js
@@ -1,51 +1,56 @@
 import { renderHook } from "@testing-library/react";
 import { rest } from "msw";
 import { act } from "react-dom/test-utils";
-import { AuthContext } from "../../context/AuthContext";
-import { WorkoutContext } from "../../context/WorkoutContext";
 import { server } from "../../mocks/server";
 import { useSearch } from "../useSearch";
+import { Provider } from "react-redux";
+import store from "../../redux/store";
+
+let wrapper;
+let dispatch;
+let mockUser;
+
+beforeAll(() => {
+  wrapper = ({ children }) => {
+    return (
+      <Provider store={store}>
+          {children}
+      </Provider>
+    );
+  };
+  dispatch = store.dispatch;
+  mockUser = {
+    id: "userid",
+    email: "keech@mail.yu",
+    token: "authorizationToken",
+    username: undefined,
+    profileImg: undefined,
+    tokenExpires: Date.now() + 3600000,
+  };
+})
+
+afterAll(() => {
+  wrapper = null;
+  dispatch = null;
+  mockUser = null;
+})
 
 describe("useSearch()", () => {
-  it("should return search function and all states set to default values (falsy)", () => {
-    const wrapper = ({ children }) => {
-      return (
-        <AuthContext.Provider value={{ user: {} }}>
-          <WorkoutContext.Provider value={{ workouts: [] }}>
-            {children}
-          </WorkoutContext.Provider>
-        </AuthContext.Provider>
-      );
-    };
+  it("should return search function", () => {
     const { result } = renderHook(useSearch, { wrapper });
     expect(result.current.search).toBeTruthy();
-    expect(result.current.isLoading).toBe(null);
-    expect(result.current.limit).toBe(null);
-    expect(result.current.allWorkoutsMuscleGroups.length).toBe(0);
-    expect(result.current.total).toBe(null);
-    expect(result.current.error).toBe(null);
+    expect(typeof result.current.search).toBe("function");
   });
 
   it("should set error to truthy if search was run without authorization", async () => {
-    const wrapper = ({ children }) => {
-      return (
-        <AuthContext.Provider value={{ user: null }}>
-          <WorkoutContext.Provider
-            value={{ workouts: [], dispatch: () => {} }}
-          >
-            {children}
-          </WorkoutContext.Provider>
-        </AuthContext.Provider>
-      );
-    };
     const { result } = renderHook(useSearch, { wrapper });
     await act(() => result.current.search("pu", 0));
-    expect(result.current.isLoading).toBe(false);
-    expect(result.current.error).toBeTruthy();
-    expect(result.current.error).toMatch(/not authorized/i);
+    let state = store.getState();
+    expect(state.workout.setWorkoutsError).toBeTruthy();
+    expect(state.workout.setWorkoutsError).toMatch(/not authorized/i);
   });
 
-  it("should set error to truthy if search was not successful", async () => {
+  it("should set setWorkoutsError message if search was not successful", async () => {
     server.use(
       rest.get(`${process.env.REACT_APP_API}/api/workouts/*`, (req, res, ctx) => {
         return res(
@@ -56,43 +61,69 @@ describe("useSearch()", () => {
         );
       })
     );
-    const wrapper = ({ children }) => {
-      return (
-        <AuthContext.Provider value={{ user: {} }}>
-          <WorkoutContext.Provider
-            value={{ workouts: [], dispatch: () => {} }}
-          >
-            {children}
-          </WorkoutContext.Provider>
-        </AuthContext.Provider>
-      );
-    };
+    const mockWorkout = {
+      id: "mockId",
+      title: "lunges",
+      muscle_group: "leg",
+      reps: "44",
+      load: "21",
+      user_id: "userid"
+     };
+    dispatch({type: "LOGIN_SUCCESS", payload: mockUser})
+    dispatch({type: "CREATE_WORKOUT_SUCCESS", payload: mockWorkout})
     const { result } = renderHook(useSearch, { wrapper });
-    await act(() => result.current.search("pu", 0));
-    expect(result.current.isLoading).toBe(false);
-    expect(result.current.allWorkoutsMuscleGroups.length).toBe(0);
-    expect(result.current.error).toBeTruthy();
-    expect(result.current.error).toMatch(/something went wrong/i);
+    await act(() => result.current.search("lu", 0));
+    let state = store.getState();
+    expect(state.workout.setWorkoutsError).toBeTruthy();
+    expect(state.workout.setWorkoutsError).toMatch(/something went wrong/i);
+    dispatch({type: "DELETE_ALL_WORKOUTS_SUCCESS", payload: "success"});
+    dispatch({type: "LOGOUT"});
   });
 
-  it("should run search function and update total and limit states to truthy", async () => {
-    const wrapper = ({ children }) => {
-      return (
-        <AuthContext.Provider value={{ user: {} }}>
-          <WorkoutContext.Provider
-            value={{ workouts: [], dispatch: () => {} }}
-          >
-            {children}
-          </WorkoutContext.Provider>
-        </AuthContext.Provider>
-      );
-    };
+  it("should update workout state with search result workouts given that user is authorized", async () => {
+    const mockWorkouts = [{
+      id: "mockId1",
+      title: "lunges",
+      muscle_group: "leg",
+      reps: "44",
+      load: "21",
+      user_id: "userid"
+     },
+     {
+      id: "mockId2",
+      title: "pushups",
+      muscle_group: "chest",
+      reps: "44",
+      load: "21",
+      user_id: "userid"
+    }
+   ];
+    server.use(
+      rest.get(`${process.env.REACT_APP_API}/api/workouts/*`, (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            workoutsChunk: [...mockWorkouts.slice(0,2)],
+            allUserWorkoutsMuscleGroups: [...mockWorkouts.map(m => m.muscle_group)],
+            total: 2,
+            limit: 3,
+            noWorkoutsByQuery: false,
+          })
+        );
+      }),
+    )
+     dispatch({type: "LOGIN_SUCCESS", payload: mockUser});
+     dispatch({type: "CREATE_WORKOUT_SUCCESS", payload: mockWorkouts[0]});
+     dispatch({type: "CREATE_WORKOUT_SUCCESS", payload: mockWorkouts[1]});
+     let state = store.getState();
+     expect(state.workout.workouts.total).toBe(2);
+     expect(state.workout.workouts.workoutsChunk[0].title).toMatch(mockWorkouts[1].title);
+     expect(state.workout.workouts.workoutsChunk[1].title).toMatch(mockWorkouts[0].title);
     const { result } = renderHook(useSearch, { wrapper });
-    await act(() => result.current.search("pu", 0));
-    expect(result.current.isLoading).toBe(false);
-    expect(result.current.error).toBe(null);
-    expect(result.current.allWorkoutsMuscleGroups.length).not.toBe(0);
-    expect(result.current.limit).toBeTruthy();
-    expect(result.current.total).toBeTruthy();
+    await act(() => result.current.search(mockWorkouts[1].title.slice(0,1), 0));
+    state = store.getState();
+    expect(state.workout.workouts.workoutsChunk[0].title).toBe(mockWorkouts[0].title);
+    dispatch({type: "DELETE_ALL_WORKOUTS_SUCCESS", payload: "success"});
+    dispatch({type: "LOGOUT"});
   });
 });
