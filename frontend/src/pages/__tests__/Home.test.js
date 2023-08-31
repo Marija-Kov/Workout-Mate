e2be46eb33330e08bdc5ae83df558e3b53a14847
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-08-31 22:05:51 +0200

    Update with redux provider

--- frontend/src/pages/__tests__/Home.test.js
+++ frontend/src/pages/__tests__/Home.test.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { render, screen, cleanup } from "@testing-library/react";
 import user from "@testing-library/user-event";
-import { AuthContext } from "../../context/AuthContext";
-import { WorkoutContext } from "../../context/WorkoutContext";
 import { jest } from "@jest/globals"
 import { genSampleWorkouts } from '../../utils/test/genSampleWorkouts';
 import Home from "../Home";
 import Search from '../../components/Search';
 import { Chart } from '../../components/Chart';
+import { ChartPlaceholder } from '../../components/ChartPlaceholder';
 import Pagination from '../../components/Pagination';
 import WorkoutDetails from '../../components/WorkoutDetails';
 import WorkoutForm from '../../components/WorkoutForm';
+import { Provider } from 'react-redux';
+import store from '../../redux/store';
 
+
+jest.mock('../../components/ChartPlaceholder')
 jest.mock('../../components/Chart')
 jest.mock("../../components/WorkoutDetails");
 jest.mock("../../components/Pagination");
@@ -20,20 +23,18 @@ jest.mock("../../components/Search");
 jest.mock("../../hooks/useSearch", () => ({
   useSearch: () => {
        return {
-           search: () => {},
-           total: mockWorkouts.searchResults.length,
-           limit: mockWorkouts.resultsOnPage.length,
-           allWorkoutsMuscleGroups: mockWorkouts.allWorkoutsMuscleGroups,
-           isLoading: false,
+           search: () => {}
        };
   }
         
 }));
 
 let mockWorkouts;
+let dispatch;
 
 beforeAll(() => {
-  mockWorkouts = genSampleWorkouts()
+  mockWorkouts = genSampleWorkouts();
+  dispatch = store.dispatch;
 });
 
 afterEach(() => {
@@ -43,18 +44,17 @@ afterEach(() => {
 afterAll(() => {
   jest.clearAllMocks();
   mockWorkouts = null;
+  dispatch = null
 });
 
 describe("<Home />", () => {
     it("should render Home page correctly given that user is authenticated", async () => {
+      await dispatch({type: "LOGIN_SUCCESS", payload: {}});
+      await dispatch({type: "SET_WORKOUTS_SUCCESS", payload: mockWorkouts});
       render(
-        <AuthContext.Provider value={{ user: {}}}>
-          <WorkoutContext.Provider
-            value={{ workouts: mockWorkouts.resultsOnPage }}
-          >
+        <Provider store={store}>
             <Home />
-          </WorkoutContext.Provider>
-        </AuthContext.Provider>
+        </Provider>
       );
       const addWorkoutBtn = await screen.findByLabelText(/buff it up/i);
       const workouts = await screen.findByLabelText("workouts");
@@ -64,18 +64,15 @@ describe("<Home />", () => {
       expect(WorkoutDetails).toHaveBeenCalled();
       expect(addWorkoutBtn).toBeInTheDocument();
       expect(workouts).toBeInTheDocument();
+      dispatch({type: "LOGOUT"});
     });
 
     it("should render WorkoutForm component when user clicks on 'Buff it up' button", async () => {
       user.setup();
       render(
-        <AuthContext.Provider value={{ user: {} }}>
-          <WorkoutContext.Provider
-            value={{ workouts: mockWorkouts.resultsOnPage }}
-          >
+        <Provider store={store}>
             <Home />
-          </WorkoutContext.Provider>
-        </AuthContext.Provider>
+        </Provider>
       );
       const addWorkoutBtn = await screen.findByLabelText(/buff it up/i);
       await user.click(addWorkoutBtn);
