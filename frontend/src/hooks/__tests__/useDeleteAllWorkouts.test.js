da67c7646e3bc5d6d27a2352c25707e93cd1f62d
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-08-31 10:06:56 +0200

    Update tests, fix the hook

--- frontend/src/hooks/__tests__/useDeleteAllWorkouts.test.js
+++ frontend/src/hooks/__tests__/useDeleteAllWorkouts.test.js
@@ -1,82 +1,123 @@
 import { renderHook, act } from "@testing-library/react";
 import { rest } from "msw";
 import { server } from "../../mocks/server";
-import { AuthContext } from "../../context/AuthContext";
-import { WorkoutContext } from "../../context/WorkoutContext";
 import { useDeleteAllWorkouts } from "../useDeleteAllWorkouts"
+import { Provider } from "react-redux";
+import store from '../../redux/store';
+
+let wrapper;
+let dispatch;
+let mockUser;
+let mockWorkouts;
+
+beforeAll(() => {
+  wrapper = ({ children }) => {
+    return (
+      <Provider store={store}>
+          {children}
+      </Provider>
+    );
+  };
+  dispatch = store.dispatch;
+  mockUser = {
+    id: "userid",
+    email: "keech@mail.yu",
+    token: "authorizationToken",
+    username: undefined,
+    profileImg: undefined,
+    tokenExpires: Date.now() + 3600000,
+  };
+  mockWorkouts =[{
+    _id: "mockId1",
+    title: "lunges",
+    muscle_group: "leg",
+    reps: "44",
+    load: "21",
+    user_id: "userid"
+   },
+   {
+    _id: "mockId2",
+    title: "situps",
+    muscle_group: "ab",
+    reps: "44",
+    load: "21",
+    user_id: "userid"
+   }] ;
+})
+
+afterAll(() => {
+  wrapper = null;
+  dispatch = null;
+  mockUser = null;
+  mockWorkouts = null;
+})
 
 describe("useDeleteAllWorkouts()", () => {
-  it("should return deleteAllWorkouts function and error with delault state (false)", () => {
-      const wrapper = ({ children }) => {
-        return (
-          <AuthContext.Provider value={{ user: {} }}>
-            <WorkoutContext.Provider value={{ workouts: [] }}>
-              {children}
-            </WorkoutContext.Provider>
-          </AuthContext.Provider>
-        );
-      };
+  it("should return deleteAllWorkouts function", () => {
     const { result } = renderHook(useDeleteAllWorkouts, { wrapper });
     expect(result.current.deleteAllWorkouts).toBeTruthy();
-    expect(result.current.error).toBeFalsy();
+    expect(typeof result.current.deleteAllWorkouts).toBe("function");
   });
 
-  it("should not change default error state (false) when deleteAllWorkouts was run with authorization", async () => {
-    const wrapper = ({ children }) => {
-        return (
-          <AuthContext.Provider value={{ user: {} }}>
-            <WorkoutContext.Provider value={{ workouts: [], dispatch: () => {} }}>
-              {children}
-            </WorkoutContext.Provider>
-          </AuthContext.Provider>
-        );
-      };
+  it("should delete all workouts given that deleteAllWorkouts was run with authorization", async () => {
+    dispatch({type: "LOGIN_SUCCESS", payload: mockUser})
+    for(let i = 0; i < mockWorkouts.length; ++i){
+     dispatch({type: "CREATE_WORKOUT_SUCCESS", payload: mockWorkouts[i]}); 
+    }  
+    let state = store.getState();
+    expect(state.workout.workouts.total).toBe(2);
     const { result } = renderHook(useDeleteAllWorkouts, { wrapper });
     await act(() => result.current.deleteAllWorkouts());
-    expect(result.current.error).toBeFalsy();
+    state = store.getState();
+    expect(state.workout.workouts.total).toBe(0);
+    expect(state.workout.deleteAllWorkoutsSuccess).toBeTruthy();
+    expect(state.workout.deleteAllWorkoutsError).toBeFalsy();
+    dispatch({type: "LOGOUT"});
   });
 
-    it("should change error state to 'true' when deleteAllWorkouts was run without authorization", async () => {
-      const wrapper = ({ children }) => {
-        return (
-          <AuthContext.Provider value={{ user: null }}>
-            <WorkoutContext.Provider
-              value={{ workouts: [], dispatch: () => {} }}
-            >
-              {children}
-            </WorkoutContext.Provider>
-          </AuthContext.Provider>
-        );
-      };
+    it("should set deleteAllWorkoutsError message given that deleteAllWorkouts was run without authorization", async () => {
+      dispatch({type: "LOGIN_SUCCESS", payload: mockUser})
+      for(let i = 0; i < mockWorkouts.length; ++i){
+       dispatch({type: "CREATE_WORKOUT_SUCCESS", payload: mockWorkouts[i]}); 
+      }  
+      let state = store.getState();
+      expect(state.workout.workouts.total).toBe(2);
+      dispatch({type: "LOGOUT"});
       const { result } = renderHook(useDeleteAllWorkouts, { wrapper });
       await act(() => result.current.deleteAllWorkouts());
-      expect(result.current.error).toBeTruthy();
-      expect(result.current.error).toMatch(/must be logged in/i);
+      state = store.getState();
+      expect(state.workout.deleteAllWorkoutsSuccess).toBeFalsy();
+      expect(state.workout.deleteAllWorkoutsError).toBeTruthy();
+      expect(state.workout.deleteAllWorkoutsError).toMatch(/not authorized/i);
+      dispatch({type: "LOGIN_SUCCESS", payload: mockUser})
+      state = store.getState();
+      expect(state.workout.workouts.total).toBe(2);
+      dispatch({type:"DELETE_ALL_WORKOUTS_SUCCESS", payload: "All workouts deleted successfully"})
+      dispatch({type: "LOGOUT"});
     });
 
-    it("should change error state to 'true' when deleteAllWorkouts was not successful", async () => {
+    it("should set deleteAllWorkoutsError deleteAllWorkouts was not successful", async () => {
       server.use(
         rest.delete(`${process.env.REACT_APP_API}/api/workouts/`, (req, res, ctx) => {
           return res(
-            ctx.status(404)
+            ctx.status(500)
           );
         })
       );
-      const wrapper = ({ children }) => {
-        return (
-          <AuthContext.Provider value={{ user: {} }}>
-            <WorkoutContext.Provider
-              value={{ workouts: [], dispatch: () => {} }}
-            >
-              {children}
-            </WorkoutContext.Provider>
-          </AuthContext.Provider>
-        );
-      };
+      dispatch({type: "LOGIN_SUCCESS", payload: mockUser})
+      for(let i = 0; i < mockWorkouts.length; ++i){
+       dispatch({type: "CREATE_WORKOUT_SUCCESS", payload: mockWorkouts[i]}); 
+      }  
+      let state = store.getState();
+      expect(state.workout.workouts.total).toBe(2);
       const { result } = renderHook(useDeleteAllWorkouts, { wrapper });
       await act(() => result.current.deleteAllWorkouts());
-      expect(result.current.error).toBeTruthy();
-      expect(result.current.error).toMatch(/something went wrong/i);
+      state = store.getState();
+      expect(state.workout.deleteAllWorkoutsSuccess).toBeFalsy();
+      expect(state.workout.deleteAllWorkoutsError).toBeTruthy();
+      expect(state.workout.workouts.total).toBe(2);
+      dispatch({type:"DELETE_ALL_WORKOUTS_SUCCESS", payload: "All workouts deleted successfully"});
+      dispatch({type: "LOGOUT"});
     });
 
 });
--- frontend/src/hooks/useDeleteAllWorkouts.js
+++ frontend/src/hooks/useDeleteAllWorkouts.js
@@ -1,12 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux';
 
 export const useDeleteAllWorkouts = () => {
-  const { dispatch } = useDispatch();
+  const dispatch = useDispatch();
   const { user } =  useSelector(state => state.user);
 
   const deleteAllWorkouts = async () => {
+    dispatch({type: "DELETE_ALL_WORKOUTS_REQ"});
     if (!user) {
-      dispatch({type: "DELETE_ALL_WORKOUTS_FAIL", payload: "You must be logged in"});
+      dispatch({type: "DELETE_ALL_WORKOUTS_FAIL", payload: "Not authorized"});
       return;
     }
     const response = await fetch(`${process.env.REACT_APP_API}/api/workouts/`, {
@@ -20,7 +21,7 @@ export const useDeleteAllWorkouts = () => {
        dispatch({type:"DELETE_ALL_WORKOUTS_SUCCESS", payload: "All workouts deleted successfully"})
     }
     if (!response.ok) {
-       dispatch({dispatch: "DELETE_ALL_WORKOUTS_FAIL", payload: "Something went wrong with deleting workouts. This could be because: 1)the account was already deleted, 2)something else. Please try logging in again to make sure that your account was deleted as requested before you contact support."});
+       dispatch({type: "DELETE_ALL_WORKOUTS_FAIL", payload: "Something went wrong with deleting workouts. This could be because: 1)the account was already deleted, 2)something else. Please try logging in again to make sure that your account was deleted as requested before you contact support."});
     }
   }
 
