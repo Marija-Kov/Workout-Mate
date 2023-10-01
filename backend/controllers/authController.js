0cdbc65cb591d0ba9424d6d344dc004e73e713f4
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-10-01 22:51:31 +0200

    Refactored with ApiError

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/userModel');
 const Workout = require("../models/workoutModel");
 const jwt = require('jsonwebtoken');
 const sendEmail = require("../middleware/sendEmail");
+const { ApiError } = require('../error/error');
 
 const expiresIn = Number(process.env.AUTH_TOKEN_EXPIRES_IN);
 const createToken = (_id) => {
@@ -10,131 +11,103 @@ return jwt.sign({_id}, process.env.SECRET, {expiresIn: expiresIn})
 
 module.exports.signup_post = async (req, res) => {
  const {email, password} = req.body;
-     try {
  const user = await User.signup(email, password);
  const id = user._id;
  const confirmationToken = createToken(id);
  user.accountConfirmationToken = confirmationToken;
  user.accountConfirmationTokenExpires = Date.now() + 3600000;
  await user.save();
-
  const registeredUsers = await User.find({});
  const limit =
    process.env.NODE_ENV !== "test"
      ? Number(process.env.MAX_USERS)
      : Number(process.env.TEST_MAX_USERS);
- if (registeredUsers.length === limit) {
+ if(registeredUsers.length === limit){
    const id = registeredUsers[0]._id;
    await User.findOneAndDelete({ _id: id });
    await Workout.deleteMany({user_id: id});
  }
-
  const clientUrl = process.env.CLIENT_URL;
  const accountVerificationLink = `${clientUrl}/users?accountConfirmationToken=${confirmationToken}`;
-
-   sendEmail(
-     user.email,
-     "Verify your account",
-     {
-       link: accountVerificationLink,
-     },
-     "../templates/verifySignup.handlebars"
-   );
-
-  res.status(200).json({id, token: confirmationToken, success: "Account created and pending confirmation. Please check your inbox."});
-  } catch(err){
-  res.status(400).json({error: err.message});
-  }
+ sendEmail(
+   user.email,
+   "Verify your account",
+   {
+     link: accountVerificationLink,
+   },
+   "../templates/verifySignup.handlebars"
+ );
+ res.status(200).json({id, token: confirmationToken, success: "Account created and pending confirmation. Please check your inbox."});
 }
 
 module.exports.verify_user = async (req, res) => {
   const {accountConfirmationToken} = req.params;
   if(!accountConfirmationToken){
-    res.status(404).json({ error: "Account confirmation token not found"})
+    ApiError.badInput("Account confirmation token not found")
   }
-  try {
    const user = await User.findOne({
     accountConfirmationToken: accountConfirmationToken,
   });
   if(!user){
-    return res.status(404).json({error: "Couldn't find user with provided confirmation token - this might be because the account has already been confirmed"})
+    ApiError.badInput("Couldn't find user with provided confirmation token - this might be because the account has already been confirmed")
   }
   user.accountStatus = "active";
   user.accountConfirmationToken = undefined;
   user.accountConfirmationTokenExpires = undefined;
   await user.save()  
-   res.status(200).json({id: user._id, success: "Success! You may log in with your account now."})
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+  res.status(200).json({id: user._id, success: "Success! You may log in with your account now."})
 }
 
 module.exports.login_post = async (req, res) => {
-    const {email, password} = req.body;
-    try{
-      const user = await User.login(email, password);
-      const id = user._id;
-      const username = user.username;
-      const profileImg = user.profileImg;
-      const token = createToken(id);
-      const tokenExpires = Date.now() + expiresIn*1000;
-       res.status(200).json({id, email, token, username, profileImg, tokenExpires});
-    } catch(err){
-      res.status(400).json({error: err.message});
-    }
+  const {email, password} = req.body;
+  const user = await User.login(email, password);
+  const id = user._id;
+  const username = user.username;
+  const profileImg = user.profileImg;
+  const token = createToken(id);
+  const tokenExpires = Date.now() + expiresIn*1000;
+  res.status(200).json({id, email, token, username, profileImg, tokenExpires});
+
 }
 
 module.exports.user_update_patch = async (req, res) => {
   if(!req.user){
-    res.status(401).json({error: "Not authorized"})
-    return
+    ApiError.badInput("Not authorized")
   };
   if(req.body.username && req.body.username.length > 12){
-    res.status(400).json({error: "Too long name."})
-    return
+    ApiError.badInput("Too long name")
   }
   if(req.body.profileImg && 
      !req.body.profileImg.match(/^data:image\/jpeg/) &&
      !req.body.profileImg.match(/^data:image\/png/) &&
      !req.body.profileImg.match(/^data:image\/svg/) 
      ){
-    res.status(400).json({error: "Bad input, must be JPEG, PNG or SVG image format"})
-    return
+    ApiError.badInput("Bad input, must be JPEG, PNG or SVG image format")
   }
-
   const {id} = req.params;
   const token = req.headers.authorization.slice(7);
-  try {
-   const user = await User.findOneAndUpdate({_id:id}, req.body, {new: true, runValidators: true}); 
-   res
-     .status(200)
+  const user = await User.findOneAndUpdate({_id:id}, req.body, {new: true, runValidators: true}); 
+  res.status(200)
      .json({
       user: {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-        profileImg: user.profileImg,
-        token: token,
-      },
-       success: "Profile updated.",
-     });
-  } catch (error) {
-    res.status(500).json({ error: "Something went wrong. Please refresh and try again.", logError: error.message });
-  }
+       id: user.id,
+       email: user.email,
+       username: user.username,
+       profileImg: user.profileImg,
+       token: token,
+     },
+      success: "Profile updated.",
+    });
+
 };
 
 module.exports.user_deletion = async (req, res) => {
   const { id } = req.params;
   if(!id){
-      res.status(401).json({error: "Not authorized"})
-    return
+    ApiError.badInput("Not authorized")
   }
-  try {
-    const user = await User.findOneAndDelete({ _id: id });
-   res.status(200).json({success: "Account deleted successfully"});
-  } catch (error) {
-    res.status(500).json({error: "Something went wrong. Please try later.", logError: error.message})
-  }
+  await User.findOneAndDelete({ _id: id });
+  res.status(200).json({success: "Account deleted successfully"});
 };
 
 
