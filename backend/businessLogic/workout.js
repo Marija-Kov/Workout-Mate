38e57d17acb0e943f692485d20af7233a583b244
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-11-23 19:37:11 +0100

    Decoupled workout business logic from database-specific details

--- backend/businessLogic/workout.js
+++ backend/businessLogic/workout.js
@@ -1,6 +1,7 @@
-const Workout = require("../models/workoutModel");
-const mongoose = require("mongoose");
+const WorkoutRepository = require("../dataAccessLayer/workoutRepository");
 const { ApiError } = require("../error/error");
+
+const Workout = new WorkoutRepository();
 
 const getAllWorkouts = async (user, page, searchQuery) => {
   if (!user) {
@@ -8,23 +9,17 @@ const getAllWorkouts = async (user, page, searchQuery) => {
   }
   const user_id = user._id;
   const limit = 3;
-  const allUserWorkoutsMuscleGroups = (await Workout.find({ user_id })).map(
+  const allUserWorkoutsMuscleGroups = (await Workout.getAll(user_id)).map(
     (workout) => workout.muscle_group
   );
-  const allUserWorkoutsByQuery = await Workout.find(
-    searchQuery
-      ? { user_id, title: new RegExp(`^${searchQuery.toLowerCase()}`) }
-      : { user_id }
-  );
+  const allUserWorkoutsByQuery = await Workout.getByQuery(user_id, searchQuery);
   const total = allUserWorkoutsByQuery.length;
-  const workoutsChunk = await Workout.find(
-    searchQuery
-      ? { user_id, title: new RegExp(`^${searchQuery.toLowerCase()}`) }
-      : { user_id }
-  )
-    .sort({ createdAt: -1 })
-    .skip(page * limit)
-    .limit(limit);
+  const workoutsChunk = await Workout.getChunkByQuery(
+    user_id,
+    searchQuery,
+    page,
+    limit
+  );
   const pageSpread = pageSpreadHelper(allUserWorkoutsByQuery.length, limit);
   const noWorkoutsByQuery = total
     ? false
@@ -59,28 +54,22 @@ const addWorkout = async (title, muscleGroup, reps, load, user) => {
     ApiError.badInput("Load value too large");
   }
   const user_id = user._id;
-  const allWorkoutsByUser = await Workout.find({ user_id });
+  const allWorkoutsByUser = await Workout.getAll(user_id);
   const limit =
     process.env.NODE_ENV !== "test"
       ? Number(process.env.MAX_WORKOUTS_PER_USER)
       : Number(process.env.TEST_MAX_WORKOUTS_PER_USER);
   if (allWorkoutsByUser.length === limit) {
-    const _id = allWorkoutsByUser[0]._id;
-    await Workout.findOneAndDelete({ _id });
+    const id = allWorkoutsByUser[0]._id;
+    await Workout.delete(id);
   }
-  const workout = await Workout.create({
-    title: title.trim().toLowerCase(),
-    muscle_group: muscleGroup,
-    reps,
-    load,
-    user_id,
-  });
+  const workout = await Workout.add(title, muscleGroup, reps, load, user_id);
   return workout;
 };
 
 const updateWorkout = async (id, body) => {
   const { title, reps, load } = body;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
+  if (!Workout.isValidId(id)) {
     ApiError.notFound("Invalid workout id");
   }
   if (title && !title.match(/^[a-zA-Z\s]*$/)) {
@@ -95,22 +84,19 @@ const updateWorkout = async (id, body) => {
   if (load && load > 9999) {
     ApiError.badInput("Load value too large");
   }
-  const workout = await Workout.findOneAndUpdate({ _id: id }, body, {
-    new: true,
-    runValidators: true,
-  });
+  const workout = await Workout.update(id, body);
   return workout;
 };
 
 const deleteWorkout = async (id) => {
-  if (!mongoose.Types.ObjectId.isValid(id)) {
+  if (!Workout.isValidId(id)) {
     ApiError.notFound("Invalid workout id");
   }
-  const workout = await Workout.findOneAndDelete({ _id: id });
+  const workout = await Workout.delete(id);
   if (!workout) {
     ApiError.notFound(`Workout id (${id}) does not exist`);
   }
-  const workouts = await Workout.find({ user_id: workout.user_id });
+  const workouts = await Workout.getAll(workout.user_id);
   return { workout: workout, remaining: workouts.length };
 };
 
@@ -119,7 +105,7 @@ const deleteAllWorkouts = async (user) => {
     ApiError.notAuthorized("Not authorized");
   }
   const user_id = user._id;
-  const workouts = await Workout.deleteMany({ user_id });
+  const workouts = await Workout.deleteAll(user_id);
   return workouts;
 };
 
