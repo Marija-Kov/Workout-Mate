7a7eb54e77ce2816b91b4bd96592af61966a9e0d
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-11-24 14:20:38 +0100

    Changed function name

--- backend/controllers/passwordResetController.js
+++ backend/controllers/passwordResetController.js
@@ -3,7 +3,7 @@ const {
   resetPassword,
 } = require("../businessLogic/passwordReset");
 
-module.exports.reset_password_request = async (req, res) => {
+module.exports.forgot_password = async (req, res) => {
   const { email } = req.body;
   const result = await forgotPassword(email);
   return res.status(200).json(result);
--- backend/routes/resetPassword.js
+++ backend/routes/resetPassword.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passwordResetController = require("../controllers/passwordResetController");
 const { tryCatch } = require("../error/tryCatch");
 
-router.post("/", tryCatch(passwordResetController.reset_password_request));
+router.post("/", tryCatch(passwordResetController.forgot_password));
 router.get("/:token", tryCatch(passwordResetController.reset_password));
 router.patch("/:token", tryCatch(passwordResetController.reset_password));
 
