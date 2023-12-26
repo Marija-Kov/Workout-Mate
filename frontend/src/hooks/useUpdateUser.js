dd0972095fef24a682b77b1d9d1a774a3052ac53
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-12-26 10:27:57 +0100

    Added buffer module

--- frontend/src/hooks/useUpdateUser.js
+++ frontend/src/hooks/useUpdateUser.js
@@ -1,4 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
+import { Buffer } from "buffer";
 
 export const useUpdateUser = () => {
   const { user } = useSelector((state) => state.user);
