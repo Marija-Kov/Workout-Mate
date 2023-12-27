02a15d04928cef9c6e5313b559b1840e5201090f
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-12-27 07:57:08 +0100

    Cleanup, maintenance

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -83,26 +83,3 @@ userSchema.statics.login = async function (email, password) {
 };
 
 module.exports = mongoose.model("User", userSchema);
-
-// This function is fired AFTER the document is saved to db:
-// userSchema.post('save', function (doc, next) {
-//     console.log('new user created and saved', doc);
-//     next(); // *
-// })
-
-// Fired BEFORE the document is saved to db:
-// userSchema.pre('save', async function (next) {  // **
-//    const salt = await bcrypt.genSalt();
-//    this.password = await bcrypt.hash(this.password, salt);
-// // ***
-//     next();
-// })
-
-// * next has to be called at the end of any mongoose middleware/hook or the process will hang and the response will not be sent to the client
-
-// ** Why not ()=> ? not using arrow function because it doesn't bind 'this',
-// which we want to use to refer to the instance of the user that hasn't been created YET.
-
-// ** a) We don't pass the 'doc' argument here because at the time the function is fired, the doc doesn't exist in the db yet.
-
-// *** We can refer to the instance of the user here with 'this' (not 'User') because we have access to it LOCALLY before it's saved to the db .
--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
-// Schema is like a mongoose object property.
 
 const workoutSchema = new Schema(
   {
