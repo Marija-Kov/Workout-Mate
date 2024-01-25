626a22432ca7d13303eafe795ff594129095b2d7
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2024-01-25 20:13:00 +0100

    Fixed unexpected behaviour when updating to 'forearm and grip' muscle group..
     - Corrected the option value in WorkoutForm (this wasn't the direct cause of the issue)
    
     - Ensured that the routine balance reducer matched the payload (muscleGroups) property ('forearm and grip') with the property read by Chart.js ('forearmAndGrip') when updating the routine balance state.

--- frontend/src/redux/routineBalance/routineBalanceReducer.js
+++ frontend/src/redux/routineBalance/routineBalanceReducer.js
@@ -2,30 +2,19 @@ const SET_ROUTINE_BALANCE = "SET_ROUTINE_BALANCE";
 const RESET_ROUTINE_BALANCE_STATE = "SET_ROUTINE_BALANCE_STATE";
 
 const init = {
-  chest: 0,
-  shoulder: 0,
-  biceps: 0,
-  triceps: 0,
-  leg: 0,
-  back: 0,
-  glute: 0,
   ab: 0,
+  back: 0,
+  biceps: 0,
   calf: 0,
+  chest: 0,
   forearmAndGrip: 0,
+  glute: 0,
+  leg: 0,
+  shoulder: 0,
+  triceps: 0,
 };
 
-const allMuscleGroups = [
-  "chest",
-  "shoulder",
-  "biceps",
-  "triceps",
-  "leg",
-  "back",
-  "glute",
-  "ab",
-  "calf",
-  "forearmAndGrip",
-];
+const allMuscleGroups = Object.keys(init);
 const len = allMuscleGroups.length;
 
 export const routineBalanceReducer = (state = init, action) => {
@@ -36,10 +25,14 @@ export const routineBalanceReducer = (state = init, action) => {
       const total = muscleGroups.length;
       const balance = {};
       for (let i = 0; i < len; ++i) {
-        const group =
-          (muscleGroups.filter((e) => e === allMuscleGroups[i]).length * 100) /
+        const group = allMuscleGroups[i];
+        const percent =
+          (muscleGroups.filter(
+            (e) => e.split(" ").join("") === group.toLowerCase()
+          ).length *
+            100) /
           total;
-        balance[allMuscleGroups[i]] = group.toFixed(1);
+        balance[group] = percent.toFixed(1);
       }
       return balance;
     case RESET_ROUTINE_BALANCE_STATE:
