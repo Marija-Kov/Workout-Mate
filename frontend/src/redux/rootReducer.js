267222cd64fc6f86e5c663876bd5a16b955901e3
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2024-03-06 22:56:37 +0100

    Moved loading and error messages outside user and workout states..
    -Defined loader state;
    
    Reduced number of actions for workouts and user states;
    
    Renamed actions for conciseness
    
    Eliminated annoying nesting (state.workout.workouts, state.user.user).

--- frontend/src/redux/rootReducer.js
+++ frontend/src/redux/rootReducer.js
@@ -6,15 +6,17 @@ import { queryReducer } from "./query/queryReducer";
 import { toggleMountComponentsReducer } from "./toggleMountComponents/toggleMountComponentsReducer";
 import { routineBalanceReducer } from "./routineBalance/routineBalanceReducer";
 import { flashMessagesReducer } from "./flashMessages/flashMessagesReducer";
+import { loaderReducer } from "./loader/loaderReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
-  workout: workoutReducer,
+  workouts: workoutReducer,
   page: pageReducer,
   query: queryReducer,
   toggleMountComponents: toggleMountComponentsReducer,
   routineBalance: routineBalanceReducer,
-  flashMessages: flashMessagesReducer
+  flashMessages: flashMessagesReducer,
+  loader: loaderReducer
 });
 
 export default rootReducer;
