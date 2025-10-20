5ae4929f962c199c7310abb2d3e3fe2ab62b9530
marija <kccmrj@outlook.com>
2025-10-20 07:26:25 +0200

    extract workout input validation

--- backend/src/businessLogic/workout.js
+++ backend/src/businessLogic/workout.js
@@ -24,6 +24,59 @@ const addWorkout = async (title, muscleGroup, reps, load, user_id) => {
   if (!title || !muscleGroup || !String(reps) || !String(load)) {
     ApiError.badInput("Please fill out the empty fields");
   }
+  validateWorkoutInput(title, muscleGroup, reps, load);
+  const workoutsCount = await Workout.getCount(user_id);
+  const limit = Number(process.env.MAX_WORKOUTS_PER_USER) || 30;
+  /*
+   The following block will trigger the deletion of the user's oldest workout
+   if the set limit is exceeded. This is done to avoid having to manually clear
+   the database as the intention behind the app isn't to retain users at this time.
+  */
+  if (workoutsCount === limit) {
+    const id = await Workout.getOldestEntryId(user_id);
+    await Workout.delete(id);
+  }
+  const workout = await Workout.add(
+    title.trim().toLowerCase(),
+    muscleGroup,
+    reps,
+    load,
+    user_id
+  );
+  return workout;
+};
+
+const updateWorkout = async (id, body) => {
+  const { title, muscle_group, reps, load } = body;
+  if (title && !title.match(/^[a-zA-Z\s]*$/)) {
+    ApiError.badInput("Title may contain only letters");
+  }
+  validateWorkoutInput(title, muscle_group, reps, load);
+  const workout = await Workout.update(id, body);
+  if (!workout) {
+    ApiError.notFound(`Could not find workout id: ${id}`);
+  }
+  return workout;
+};
+
+const deleteWorkout = async (id) => {
+  const workout = await Workout.delete(id);
+  if (!workout) {
+    ApiError.notFound(`Could not find workout id: ${id}`);
+  }
+  if (process.env.NODE_ENV === "test") {
+    const remaining = await Workout.getCount(workout.user_id);
+    return { workout, remaining };
+  }
+  return { workout };
+};
+
+const deleteAllWorkouts = async (user_id) => {
+  const workouts = await Workout.deleteAll(user_id);
+  return workouts;
+};
+
+function validateWorkoutInput(title, muscleGroup, reps, load) {
   if (title && !title.match(/^[a-zA-Z\s]*$/)) {
     ApiError.badInput("Title may contain only letters");
   }
@@ -59,87 +112,7 @@ const addWorkout = async (title, muscleGroup, reps, load, user_id) => {
   if (load > 9999) {
     ApiError.badInput("Load value too large");
   }
-  const workoutsCount = await Workout.getCount(user_id);
-  const limit = Number(process.env.MAX_WORKOUTS_PER_USER) || 30;
-  /*
-   The following block will trigger the deletion of the user's oldest workout
-   if the set limit is exceeded. This is done to avoid having to manually clear
-   the database as the intention behind the app isn't to retain users at this time.
-  */
-  if (workoutsCount === limit) {
-    const id = await Workout.getOldestEntryId(user_id);
-    await Workout.delete(id);
-  }
-  const workout = await Workout.add(
-    title.trim().toLowerCase(),
-    muscleGroup,
-    reps,
-    load,
-    user_id
-  );
-  return workout;
-};
-
-const updateWorkout = async (id, body) => {
-  const { title, muscle_group, reps, load } = body;
-  if (title && !title.match(/^[a-zA-Z\s]*$/)) {
-    ApiError.badInput("Title may contain only letters");
-  }
-  if (
-    muscle_group &&
-    ![
-      "chest",
-      "shoulder",
-      "biceps",
-      "triceps",
-      "leg",
-      "back",
-      "glute",
-      "ab",
-      "calf",
-      "forearm and grip",
-    ].includes(muscle_group)
-  ) {
-    ApiError.badInput("Invalid muscle group value");
-  }
-  if (title && title.length > 30) {
-    ApiError.badInput("Too long title - max 30 characters");
-  }
-  if (reps && Number(reps) != reps) {
-    ApiError.badInput("Reps must be a number");
-  }
-  if (reps > 9999) {
-    ApiError.badInput("Reps value too large");
-  }
-  if (load && Number(load) != load) {
-    ApiError.badInput("Load must be a number");
-  }
-  if (load > 9999) {
-    ApiError.badInput("Load value too large");
-  }
-  const workout = await Workout.update(id, body);
-  if (!workout) {
-    ApiError.notFound(`Could not find workout id: ${id}`);
-  }
-  return workout;
-};
-
-const deleteWorkout = async (id) => {
-  const workout = await Workout.delete(id);
-  if (!workout) {
-    ApiError.notFound(`Could not find workout id: ${id}`);
-  }
-  if (process.env.NODE_ENV === "test") {
-    const remaining = await Workout.getCount(workout.user_id);
-    return { workout, remaining };
-  }
-  return { workout };
-};
-
-const deleteAllWorkouts = async (user_id) => {
-  const workouts = await Workout.deleteAll(user_id);
-  return workouts;
-};
+}
 
 module.exports = {
   getAllWorkouts,
